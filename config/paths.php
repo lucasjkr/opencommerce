3c4360be865f3063c86341ea7c8509cf2601dcd7
lucasjkr <lucas@lucaskrupinski.com>
2017-10-03 20:22:19 -0400

    Default /config/paths.php paths file
    Paths.php file is provided by default, but advanced users can make modifications to it.

---
+++ config/paths.php
@@ -0,0 +1,19 @@
+<?php
+// Any modifications to this file are at your own risk!
+define('DIR_SYSTEM',        DIR_ROOT    . 'system/');
+define('DIR_STORAGE',       DIR_SYSTEM  . 'storage/');
+define('DIR_CONFIG',        DIR_SYSTEM  . 'config/');
+define('DIR_CACHE',         DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD',      DIR_STORAGE . 'download/');
+define('DIR_LOGS',          DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION',  DIR_STORAGE . 'modification/');
+define('DIR_SESSION',       DIR_STORAGE . 'session/');
+define('DIR_UPLOAD',        DIR_STORAGE . 'upload/');
+
+// these are used by catalog
+define('HTTP_ROOT',         '/');
+define('HTTP_SERVER',       '/');
+
+// these are used by admin
+define('HTTP_ADMIN',       '/admin/');
+define('HTTP_CATALOG',     '/');
