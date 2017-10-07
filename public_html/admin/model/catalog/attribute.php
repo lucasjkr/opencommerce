3ce0f4f9cf52b72ef84692f33a19f4dfac2a6bc8
lucasjkr <lucas@lucaskrupinski.com>
2017-10-07 15:52:10 -0400

    Admin/Model/Catalog/Attributes - queries are now parameterized

--- public_html/admin/model/catalog/attribute.php
+++ public_html/admin/model/catalog/attribute.php
@@ -1,48 +1,88 @@
 <?php
 class ModelCatalogAttribute extends Model {
 	public function addAttribute($data) {
-		$this->db->query("INSERT INTO oc_attribute SET attribute_group_id = '" . (int)$data['attribute_group_id'] . "', sort_order = '" . (int)$data['sort_order'] . "'");
+		$this->db->query("INSERT INTO oc_attribute SET attribute_group_id = :attribute_group_id, sort_order = :sort_order'",
+            [
+                ':attribute_group_id' => $data['attribute_group_id'],
+                ':sort_order' => $data['sort_order'],
+
+            ]);
 
 		$attribute_id = $this->db->getLastId();
 
 		foreach ($data['attribute_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO oc_attribute_description SET attribute_id = '" . (int)$attribute_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			$this->db->query("INSERT INTO oc_attribute_description SET attribute_id = :attribute_id, language_id = :language_id, name = :name ",
+                [
+                    ':attribute_id' => $attribute_id,
+                    ':language_id' => $language_id,
+                    ':name' => $value['name'],
+                ]);
 		}
 
 		return $attribute_id;
 	}
 
 	public function editAttribute($attribute_id, $data) {
-		$this->db->query("UPDATE oc_attribute SET attribute_group_id = '" . (int)$data['attribute_group_id'] . "', sort_order = '" . (int)$data['sort_order'] . "' WHERE attribute_id = '" . (int)$attribute_id . "'");
+		$this->db->query("UPDATE oc_attribute SET attribute_group_id = :attribute_group_id, sort_order = :sort_order WHERE attribute_id = :attribute_id",
+            [
+                ':attribute_id' => $attribute_id,
+                ':language_id' => $language_id,
+                ':attribute_id' => $attribute_id,
+            ]);
 
-		$this->db->query("DELETE FROM oc_attribute_description WHERE attribute_id = '" . (int)$attribute_id . "'");
+		$this->db->query("DELETE FROM oc_attribute_description WHERE attribute_id = :attribute_id",
+            [
+                ':attribute_id' => $attribute_id,
+            ]);
 
 		foreach ($data['attribute_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO oc_attribute_description SET attribute_id = '" . (int)$attribute_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			$this->db->query("INSERT INTO oc_attribute_description SET attribute_id = :attribute_id, language_id = :language_id, name = :name",
+                [
+                    ':attribute_id' => $attribute_id,
+                    ':language_id' => $language_id,
+                    ':name' => $value['name'],
+                ]);
 		}
 	}
 
 	public function deleteAttribute($attribute_id) {
-		$this->db->query("DELETE FROM oc_attribute WHERE attribute_id = '" . (int)$attribute_id . "'");
-		$this->db->query("DELETE FROM oc_attribute_description WHERE attribute_id = '" . (int)$attribute_id . "'");
+		$this->db->query("DELETE FROM oc_attribute WHERE attribute_id = :attribute_id",
+            [
+                ':attribute_id' => $attribute_id
+            ]);
+		$this->db->query("DELETE FROM oc_attribute_description WHERE attribute_id = :attribute_id",
+            [
+                ':attribute_id' => $attribute_id
+            ]);
 	}
 
 	public function getAttribute($attribute_id) {
-		$query = $this->db->query("SELECT * FROM oc_attribute a LEFT JOIN oc_attribute_description ad ON (a.attribute_id = ad.attribute_id) WHERE a.attribute_id = '" . (int)$attribute_id . "' AND ad.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT * FROM oc_attribute a LEFT JOIN oc_attribute_description ad ON (a.attribute_id = ad.attribute_id) WHERE a.attribute_id = :attribute_id AND ad.language_id = :language_id",
+            [
+                ':attribute_id' => $attribute_id,
+                ':language_id' => $this->config->get('config_language_id'),
+            ]);
 
 		return $query->row;
 	}
 
 	public function getAttributes($data = array()) {
-		$sql = "SELECT *, (SELECT agd.name FROM oc_attribute_group_description agd WHERE agd.attribute_group_id = a.attribute_group_id AND agd.language_id = '" . (int)$this->config->get('config_language_id') . "') AS attribute_group FROM oc_attribute a LEFT JOIN oc_attribute_description ad ON (a.attribute_id = ad.attribute_id) WHERE ad.language_id = '" . (int)$this->config->get('config_language_id') . "'";
+        $sql = "SELECT *, (SELECT agd.name FROM oc_attribute_group_description agd WHERE agd.attribute_group_id = a.attribute_group_id AND agd.language_id = :language_id ) AS attribute_group FROM oc_attribute a LEFT JOIN oc_attribute_description ad ON (a.attribute_id = ad.attribute_id) WHERE ad.language_id = :language_id ";
+		$args[':language_id'] = $this->config->get('config_language_id');
 
-		if (!empty($data['filter_name'])) {
-			$sql .= " AND ad.name LIKE '" . $this->db->escape((string)$data['filter_name']) . "%'";
-		}
+        if (!empty($data['filter_name'])) {
+			$sql .= " AND ad.name LIKE :filter_name";
+            $args[':filter_name'] = $data['filter_name'] . '%';
+		} else {
+		    $data['filter_name'] = '';
+        }
 
 		if (!empty($data['filter_attribute_group_id'])) {
-			$sql .= " AND a.attribute_group_id = '" . (int)$data['filter_attribute_group_id'] . "'";
-		}
+			$sql .= " AND a.attribute_group_id = :filter_attribute_group_id ";
+            $args[':filter_attribute_group_id'] = $data['filter_attribute_group_id'];
+		} else {
+            $data['filter_attribute_group_id'] = '';
+        }
 
 		$sort_data = array(
 			'ad.name',
@@ -74,7 +114,7 @@ class ModelCatalogAttribute extends Model {
 			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 		}
 
-		$query = $this->db->query($sql);
+		$query = $this->db->query($sql, $args);
 
 		return $query->rows;
 	}
@@ -82,7 +122,10 @@ class ModelCatalogAttribute extends Model {
 	public function getAttributeDescriptions($attribute_id) {
 		$attribute_data = array();
 
-		$query = $this->db->query("SELECT * FROM oc_attribute_description WHERE attribute_id = '" . (int)$attribute_id . "'");
+		$query = $this->db->query("SELECT * FROM oc_attribute_description WHERE attribute_id = :attribute_id",
+            [
+                ':attribute_id' => $attribute_id
+            ]);
 
 		foreach ($query->rows as $result) {
 			$attribute_data[$result['language_id']] = array('name' => $result['name']);
@@ -98,7 +141,10 @@ class ModelCatalogAttribute extends Model {
 	}
 
 	public function getTotalAttributesByAttributeGroupId($attribute_group_id) {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_attribute WHERE attribute_group_id = '" . (int)$attribute_group_id . "'");
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_attribute WHERE attribute_group_id = :attribute_group_id",
+            [
+                ':attribute_group_id' => $attribute_group_id
+            ]);
 
 		return $query->row['total'];
 	}
