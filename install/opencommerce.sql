b8ec6b8453ec7915d37eb064e3b451ed78ac35fb
lucasjkr <lucas@lucaskrupinski.com>
2017-10-07 15:51:41 -0400

    Installation fix
    Fix inability to modify theme in settings - this was due to the oc_extention table being empty. Fixed

--- install/opencommerce.sql
+++ install/opencommerce.sql
@@ -1533,7 +1533,47 @@ CREATE TABLE `oc_extension` (
   PRIMARY KEY (`extension_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+INSERT INTO `oc_extension` (`extension_id`, `type`, `code`) VALUES
+(1, 'payment', 'cod'),
+(2, 'total', 'shipping'),
+(3, 'total', 'sub_total'),
+(4, 'total', 'tax'),
+(5, 'total', 'total'),
+(6, 'module', 'banner'),
+(7, 'module', 'carousel'),
+(8, 'total', 'credit'),
+(9, 'shipping', 'flat'),
+(10, 'total', 'handling'),
+(11, 'total', 'low_order_fee'),
+(12, 'total', 'coupon'),
+(13, 'module', 'category'),
+(14, 'module', 'account'),
+(15, 'total', 'reward'),
+(16, 'total', 'voucher'),
+(17, 'payment', 'free_checkout'),
+(18, 'module', 'featured'),
+(19, 'module', 'slideshow'),
+(20, 'theme', 'default'),
+(21, 'dashboard', 'activity'),
+(22, 'dashboard', 'sale'),
+(23, 'dashboard', 'recent'),
+(24, 'dashboard', 'order'),
+(25, 'dashboard', 'online'),
+(26, 'dashboard', 'map'),
+(27, 'dashboard', 'customer'),
+(28, 'dashboard', 'chart'),
+(29, 'report', 'sale_coupon'),
+(31, 'report', 'customer_search'),
+(32, 'report', 'customer_transaction'),
+(33, 'report', 'product_purchased'),
+(34, 'report', 'product_viewed'),
+(35, 'report', 'sale_return'),
+(36, 'report', 'sale_order'),
+(37, 'report', 'sale_shipping'),
+(38, 'report', 'sale_tax'),
+(39, 'report', 'customer_activity'),
+(40, 'report', 'customer_order'),
+(41, 'report', 'customer_reward');
 
 # Dump of table oc_extension_install
 # ------------------------------------------------------------
