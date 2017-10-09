6236ee470cf4a72c82b257a1444b53ee3d2f72d3
lucasjkr <lucas@lucaskrupinski.com>
2017-10-09 10:26:14 -0400

    Admin / Catalog / Filters model has been parameterized

--- public_html/admin/model/catalog/filter.php
+++ public_html/admin/model/catalog/filter.php
@@ -1,22 +1,40 @@
 <?php
 class ModelCatalogFilter extends Model {
 	public function addFilter($data) {
-		$this->db->query("INSERT INTO `oc_filter_group` SET sort_order = '" . (int)$data['sort_order'] . "'");
+		$this->db->query("INSERT INTO `oc_filter_group` SET `sort_order` = :sort_order",
+            [
+                ':sort_order' => $data['sort_order']
+            ]);
 
 		$filter_group_id = $this->db->getLastId();
 
 		foreach ($data['filter_group_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO oc_filter_group_description SET filter_group_id = '" . (int)$filter_group_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			$this->db->query("INSERT INTO `oc_filter_group_description` SET `filter_group_id` = :filter_group_id, `language_id` =, `name` = :name",
+                [
+                    ':filter_group_id' => $filter_group_id,
+                    ':language_id' => $language_id,
+                    ':name' => $value['name']
+                ]);
 		}
 
 		if (isset($data['filter'])) {
 			foreach ($data['filter'] as $filter) {
-				$this->db->query("INSERT INTO oc_filter SET filter_group_id = '" . (int)$filter_group_id . "', sort_order = '" . (int)$filter['sort_order'] . "'");
+				$this->db->query("INSERT INTO `oc_filter` SET `filter_group_id` = :filter_group_id, `sort_order` = :sort_order",
+                    [
+                        ':filter_group_id' => $filter_group_id,
+                        ':sort_order' => $filter['sort_order']
+                    ]);
 
 				$filter_id = $this->db->getLastId();
 
 				foreach ($filter['filter_description'] as $language_id => $filter_description) {
-					$this->db->query("INSERT INTO oc_filter_description SET filter_id = '" . (int)$filter_id . "', language_id = '" . (int)$language_id . "', filter_group_id = '" . (int)$filter_group_id . "', name = '" . $this->db->escape($filter_description['name']) . "'");
+					$this->db->query("INSERT INTO `oc_filter_description` SET `filter_id` = :filter_id, `language_id` = :language_id, `filter_group_id` = :filter_group_id, `name` = :name",
+                        [
+                            ':filter_id' => $filter_id,
+                            ':language_id' => $language_id,
+                            ':filter_group_id' => $filter_group_id,
+                            ':name' => $filter_description['name']
+                        ]);
 				}
 			}
 		}
@@ -25,49 +43,101 @@ class ModelCatalogFilter extends Model {
 	}
 
 	public function editFilter($filter_group_id, $data) {
-		$this->db->query("UPDATE `oc_filter_group` SET sort_order = '" . (int)$data['sort_order'] . "' WHERE filter_group_id = '" . (int)$filter_group_id . "'");
+		$this->db->query("UPDATE `oc_filter_group` SET `sort_order` = :sort_order WHERE `filter_group_id` = :filter_group_id ",
+            [
+                ':sort_order' => $data['sort_order'],
+                ':filter_group_id' => $filter_group_id
+            ]);
 
-		$this->db->query("DELETE FROM oc_filter_group_description WHERE filter_group_id = '" . (int)$filter_group_id . "'");
+		$this->db->query("DELETE FROM `oc_filter_group_description` WHERE `filter_group_id` = :filter_group_id",
+            [
+                ':filter_group_id' => $filter_group_id
+            ]);
 
 		foreach ($data['filter_group_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO oc_filter_group_description SET filter_group_id = '" . (int)$filter_group_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			$this->db->query("INSERT INTO `oc_filter_group_description` SET `filter_group_id` = :filter_group_id, `language_id` = :language_id, `name` = :name",
+                [
+                    ':filter_group_id' => $filter_group_id,
+                    ':language_id' => $language_id,
+                    ':name' => $value['name']
+
+                ]);
 		}
 
-		$this->db->query("DELETE FROM oc_filter WHERE filter_group_id = '" . (int)$filter_group_id . "'");
-		$this->db->query("DELETE FROM oc_filter_description WHERE filter_group_id = '" . (int)$filter_group_id . "'");
+		$this->db->query("DELETE FROM `oc_filter` WHERE `filter_group_id` = :filter_group_id",
+            [
+                ':filter_group_id' => $filter_group_id,
+            ]);
+		$this->db->query("DELETE FROM `oc_filter_description` WHERE `filter_group_id` = :filter_group_id",
+            [
+                ':filter_group_id' => $filter_group_id,
+            ]);
 
 		if (isset($data['filter'])) {
 			foreach ($data['filter'] as $filter) {
 				if ($filter['filter_id']) {
-					$this->db->query("INSERT INTO oc_filter SET filter_id = '" . (int)$filter['filter_id'] . "', filter_group_id = '" . (int)$filter_group_id . "', sort_order = '" . (int)$filter['sort_order'] . "'");
+					$this->db->query("INSERT INTO `oc_filter` SET `filter_id` = :filter_id, `filter_group_id` = :filter_group_id, `sort_order` = :sort_order",
+                        [
+                            ':filter_id' => $filter['filter_id'],
+                            ':filter_group_id' => $filter_group_id,
+                            ':sort_order' => $filter['sort_order']
+                        ]);
 				} else {
-					$this->db->query("INSERT INTO oc_filter SET filter_group_id = '" . (int)$filter_group_id . "', sort_order = '" . (int)$filter['sort_order'] . "'");
+					$this->db->query("INSERT INTO `oc_filter` SET `filter_group_id` = :filter_group_id, `sort_order` = :sort_order",
+                        [
+                            ':filter_group_id' => $filter_group_id,
+                            ':sort_order' => $filter['sort_order']
+                        ]);
 				}
 
 				$filter_id = $this->db->getLastId();
 
 				foreach ($filter['filter_description'] as $language_id => $filter_description) {
-					$this->db->query("INSERT INTO oc_filter_description SET filter_id = '" . (int)$filter_id . "', language_id = '" . (int)$language_id . "', filter_group_id = '" . (int)$filter_group_id . "', name = '" . $this->db->escape($filter_description['name']) . "'");
+					$this->db->query("INSERT INTO `oc_filter_description` SET `filter_id` = :filter_id, `language_id` = :language_id, `filter_group_id` = :filter_group_id, `name` = :name",
+                        [
+                            ':filter_id' => $filter_id,
+                            ':language_id' => $language_id,
+                            ':filter_group_id' => $filter_group_id,
+                            ':name' => $filter_description['name']
+                        ]);
 				}
 			}
 		}
 	}
 
 	public function deleteFilter($filter_group_id) {
-		$this->db->query("DELETE FROM `oc_filter_group` WHERE filter_group_id = '" . (int)$filter_group_id . "'");
-		$this->db->query("DELETE FROM `oc_filter_group_description` WHERE filter_group_id = '" . (int)$filter_group_id . "'");
-		$this->db->query("DELETE FROM `oc_filter` WHERE filter_group_id = '" . (int)$filter_group_id . "'");
-		$this->db->query("DELETE FROM `oc_filter_description` WHERE filter_group_id = '" . (int)$filter_group_id . "'");
+		$this->db->query("DELETE FROM `oc_filter_group` WHERE filter_group_id = :filter_group_id",
+            [
+                ':filter_group_id' => $filter_group_id
+            ]);
+		$this->db->query("DELETE FROM `oc_filter_group_description` WHERE filter_group_id = :filter_group_id",
+            [
+                ':filter_group_id' => $filter_group_id
+            ]);
+		$this->db->query("DELETE FROM `oc_filter` WHERE filter_group_id = :filter_group_id",
+            [
+                ':filter_group_id' => $filter_group_id
+            ]);
+		$this->db->query("DELETE FROM `oc_filter_description` WHERE filter_group_id = :filter_group_id",
+            [
+                ':filter_group_id' => $filter_group_id
+            ]);
 	}
 
 	public function getFilterGroup($filter_group_id) {
-		$query = $this->db->query("SELECT * FROM `oc_filter_group` fg LEFT JOIN oc_filter_group_description fgd ON (fg.filter_group_id = fgd.filter_group_id) WHERE fg.filter_group_id = '" . (int)$filter_group_id . "' AND fgd.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT * FROM `oc_filter_group` fg LEFT JOIN `oc_filter_group_description` fgd ON (fg.filter_group_id = fgd.filter_group_id) WHERE fg.filter_group_id = :filter_group_id AND fgd.language_id = :language_id",
+            [
+                ':filter_group_id' => $filter_group_id,
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row;
 	}
 
 	public function getFilterGroups($data = array()) {
-		$sql = "SELECT * FROM `oc_filter_group` fg LEFT JOIN oc_filter_group_description fgd ON (fg.filter_group_id = fgd.filter_group_id) WHERE fgd.language_id = '" . (int)$this->config->get('config_language_id') . "'";
+		$sql = "SELECT * FROM `oc_filter_group` fg LEFT JOIN `oc_filter_group_description` fgd ON (fg.filter_group_id = fgd.filter_group_id) WHERE fgd.language_id = :language_id";
+
+        $args[':language_id'] = $this->config->get('config_language_id');
 
 		$sort_data = array(
 			'fgd.name',
@@ -98,7 +168,7 @@ class ModelCatalogFilter extends Model {
 			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 		}
 
-		$query = $this->db->query($sql);
+		$query = $this->db->query($sql, $args);
 
 		return $query->rows;
 	}
@@ -106,7 +176,10 @@ class ModelCatalogFilter extends Model {
 	public function getFilterGroupDescriptions($filter_group_id) {
 		$filter_group_data = array();
 
-		$query = $this->db->query("SELECT * FROM oc_filter_group_description WHERE filter_group_id = '" . (int)$filter_group_id . "'");
+		$query = $this->db->query("SELECT * FROM `oc_filter_group_description` WHERE `filter_group_id` = :filter_group_id",
+            [
+                ':filter_group_id' => $filter_group_id
+            ]);
 
 		foreach ($query->rows as $result) {
 			$filter_group_data[$result['language_id']] = array('name' => $result['name']);
@@ -116,16 +189,24 @@ class ModelCatalogFilter extends Model {
 	}
 
 	public function getFilter($filter_id) {
-		$query = $this->db->query("SELECT *, (SELECT name FROM oc_filter_group_description fgd WHERE f.filter_group_id = fgd.filter_group_id AND fgd.language_id = '" . (int)$this->config->get('config_language_id') . "') AS `group` FROM oc_filter f LEFT JOIN oc_filter_description fd ON (f.filter_id = fd.filter_id) WHERE f.filter_id = '" . (int)$filter_id . "' AND fd.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT *, (SELECT `name` FROM `oc_filter_group_description` fgd WHERE f.filter_group_id = fgd.filter_group_id AND fgd.language_id = :language_id_1) AS `group` FROM oc_filter f LEFT JOIN oc_filter_description fd ON (f.filter_id = fd.filter_id) WHERE f.filter_id = :filter_id AND fd.language_id = :language_id_2",
+            [
+                ':filter_id' => $filter_id,
+                ':language_id_1' => $this->config->get('config_language_id'),
+                ':language_id_2' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row;
 	}
 
 	public function getFilters($data) {
-		$sql = "SELECT *, (SELECT name FROM oc_filter_group_description fgd WHERE f.filter_group_id = fgd.filter_group_id AND fgd.language_id = '" . (int)$this->config->get('config_language_id') . "') AS `group` FROM oc_filter f LEFT JOIN oc_filter_description fd ON (f.filter_id = fd.filter_id) WHERE fd.language_id = '" . (int)$this->config->get('config_language_id') . "'";
+		$sql = "SELECT *, (SELECT name FROM oc_filter_group_description fgd WHERE f.filter_group_id = fgd.filter_group_id AND fgd.language_id = '" . (int)$this->config->get('config_language_id') . "') AS `group` FROM oc_filter f LEFT JOIN oc_filter_description fd ON (f.filter_id = fd.filter_id) WHERE fd.language_id = :language_id";
+
+        $args[':langauge_id'] = $this->config->get('config_language_id');
 
 		if (!empty($data['filter_name'])) {
-			$sql .= " AND fd.name LIKE '" . $this->db->escape((string)$data['filter_name']) . "%'";
+			$sql .= " AND fd.name LIKE :filter_name";
+            $args[':filter_name'] = $data['filter_name'] . '%';
 		}
 
 		$sql .= " ORDER BY f.sort_order ASC";
@@ -142,7 +223,7 @@ class ModelCatalogFilter extends Model {
 			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 		}
 
-		$query = $this->db->query($sql);
+		$query = $this->db->query($sql, $args);
 
 		return $query->rows;
 	}
@@ -150,12 +231,18 @@ class ModelCatalogFilter extends Model {
 	public function getFilterDescriptions($filter_group_id) {
 		$filter_data = array();
 
-		$filter_query = $this->db->query("SELECT * FROM oc_filter WHERE filter_group_id = '" . (int)$filter_group_id . "'");
+		$filter_query = $this->db->query("SELECT * FROM `oc_filter` WHERE `filter_group_id` = :filter_group_id",
+            [
+                ':filter_group_id' => $filter_group_id
+            ]);
 
 		foreach ($filter_query->rows as $filter) {
 			$filter_description_data = array();
 
-			$filter_description_query = $this->db->query("SELECT * FROM oc_filter_description WHERE filter_id = '" . (int)$filter['filter_id'] . "'");
+			$filter_description_query = $this->db->query("SELECT * FROM `oc_filter_description` WHERE `filter_id` =  :filter_id",
+                [
+                    ':filter_id' => $filter['filter_id']
+                ]);
 
 			foreach ($filter_description_query->rows as $filter_description) {
 				$filter_description_data[$filter_description['language_id']] = array('name' => $filter_description['name']);
