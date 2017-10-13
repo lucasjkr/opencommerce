4de9373df1d34cbca463186956a79dcacb3b82c0
lucasjkr <lucas@lucaskrupinski.com>
2017-10-13 17:11:44 -0400

    Admin / Model / Customer / customer_group is parameterized

--- public_html/admin/model/customer/customer_group.php
+++ public_html/admin/model/customer/customer_group.php
@@ -1,45 +1,92 @@
 <?php
 class ModelCustomerCustomerGroup extends Model {
 	public function addCustomerGroup($data) {
-		$this->db->query("INSERT INTO oc_customer_group SET approval = '" . (int)$data['approval'] . "', sort_order = '" . (int)$data['sort_order'] . "'");
+		$this->db->query("INSERT INTO oc_customer_group SET approval = :approval, sort_order = :sort_order",
+            [
+                ':approval' => $data['approval'],
+                ':sort_order' => $data['sort_order'] ,
+            ]);
 
 		$customer_group_id = $this->db->getLastId();
 
 		foreach ($data['customer_group_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO oc_customer_group_description SET customer_group_id = '" . (int)$customer_group_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', description = '" . $this->db->escape($value['description']) . "'");
+			$this->db->query("INSERT INTO oc_customer_group_description SET customer_group_id = :customer_group_id, language_id = :language_id, name = :name, description = :description",
+                [
+                    ':customer_group_id' => $customer_group_id,
+                    ':language_id' => $language_id,
+                    ':name' => $value['name'],
+                    ':description' => $value['description']
+                ]);
 		}
 		
 		return $customer_group_id;
 	}
 
 	public function editCustomerGroup($customer_group_id, $data) {
-		$this->db->query("UPDATE oc_customer_group SET approval = '" . (int)$data['approval'] . "', sort_order = '" . (int)$data['sort_order'] . "' WHERE customer_group_id = '" . (int)$customer_group_id . "'");
+		$this->db->query("UPDATE oc_customer_group SET approval = :approval, sort_order = :sort_order WHERE customer_group_id = :customer_group_id",
+            [
+                ':approval' => $data['approval'],
+                ':sort_order' => $data['sort_order'] ,
+                ':customer_group_id' => $customer_group_id
+            ]);
 
-		$this->db->query("DELETE FROM oc_customer_group_description WHERE customer_group_id = '" . (int)$customer_group_id . "'");
+		$this->db->query("DELETE FROM oc_customer_group_description WHERE customer_group_id = :customer_group_id",
+            [
+                ':customer_group_id' => $customer_group_id
+            ]);
 
 		foreach ($data['customer_group_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO oc_customer_group_description SET customer_group_id = '" . (int)$customer_group_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', description = '" . $this->db->escape($value['description']) . "'");
+			$this->db->query("INSERT INTO oc_customer_group_description SET customer_group_id = :customer_group_id, language_id = :language_id, name = :name, description = :description",
+                [
+                    ':customer_group_id' => $customer_group_id,
+                    ':language_id' => $language_id,
+                    ':name' => $value['name'],
+                    ':description' => $value['description']
+                ]);
 		}
 	}
 
 	public function deleteCustomerGroup($customer_group_id) {
-		$this->db->query("DELETE FROM oc_customer_group WHERE customer_group_id = '" . (int)$customer_group_id . "'");
-		$this->db->query("DELETE FROM oc_customer_group_description WHERE customer_group_id = '" . (int)$customer_group_id . "'");
-		$this->db->query("DELETE FROM oc_product_discount WHERE customer_group_id = '" . (int)$customer_group_id . "'");
-		$this->db->query("DELETE FROM oc_product_special WHERE customer_group_id = '" . (int)$customer_group_id . "'");
-		$this->db->query("DELETE FROM oc_product_reward WHERE customer_group_id = '" . (int)$customer_group_id . "'");
-		$this->db->query("DELETE FROM oc_tax_rate_to_customer_group WHERE customer_group_id = '" . (int)$customer_group_id . "'");
+		$this->db->query("DELETE FROM oc_customer_group WHERE customer_group_id = :customer_group_id",
+            [
+                ':customer_group_id' => $customer_group_id
+            ]);
+		$this->db->query("DELETE FROM oc_customer_group_description WHERE customer_group_id = :customer_group_id",
+            [
+                ':customer_group_id' => $customer_group_id
+            ]);
+		$this->db->query("DELETE FROM oc_product_discount WHERE customer_group_id = :customer_group_id",
+            [
+                ':customer_group_id' => $customer_group_id
+            ]);
+		$this->db->query("DELETE FROM oc_product_special WHERE customer_group_id = :customer_group_id",
+            [
+                ':customer_group_id' => $customer_group_id
+            ]);
+		$this->db->query("DELETE FROM oc_product_reward WHERE customer_group_id = :customer_group_id",
+            [
+                ':customer_group_id' => $customer_group_id
+            ]);
+		$this->db->query("DELETE FROM oc_tax_rate_to_customer_group WHERE customer_group_id = :customer_group_id",
+            [
+                ':customer_group_id' => $customer_group_id
+            ]);
 	}
 
 	public function getCustomerGroup($customer_group_id) {
-		$query = $this->db->query("SELECT DISTINCT * FROM oc_customer_group cg LEFT JOIN oc_customer_group_description cgd ON (cg.customer_group_id = cgd.customer_group_id) WHERE cg.customer_group_id = '" . (int)$customer_group_id . "' AND cgd.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT DISTINCT * FROM oc_customer_group cg LEFT JOIN oc_customer_group_description cgd ON (cg.customer_group_id = cgd.customer_group_id) WHERE cg.customer_group_id = :customer_group_id AND cgd.language_id = :language_id",
+            [
+                ':customer_group_id' => $customer_group_id,
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row;
 	}
 
 	public function getCustomerGroups($data = array()) {
-		$sql = "SELECT * FROM oc_customer_group cg LEFT JOIN oc_customer_group_description cgd ON (cg.customer_group_id = cgd.customer_group_id) WHERE cgd.language_id = '" . (int)$this->config->get('config_language_id') . "'";
+		$sql = "SELECT * FROM oc_customer_group cg LEFT JOIN oc_customer_group_description cgd ON (cg.customer_group_id = cgd.customer_group_id) WHERE cgd.language_id = :language_id";
 
+        $args[':language_id'] = $this->config->get('config_language_id');
 		$sort_data = array(
 			'cgd.name',
 			'cg.sort_order'
@@ -69,7 +116,7 @@ class ModelCustomerCustomerGroup extends Model {
 			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 		}
 
-		$query = $this->db->query($sql);
+		$query = $this->db->query($sql, $args);
 
 		return $query->rows;
 	}
@@ -77,7 +124,10 @@ class ModelCustomerCustomerGroup extends Model {
 	public function getCustomerGroupDescriptions($customer_group_id) {
 		$customer_group_data = array();
 
-		$query = $this->db->query("SELECT * FROM oc_customer_group_description WHERE customer_group_id = '" . (int)$customer_group_id . "'");
+		$query = $this->db->query("SELECT * FROM oc_customer_group_description WHERE customer_group_id = :customer_group_id"),
+        [
+            ':customer_group_id' => $customer_group_id
+        ];
 
 		foreach ($query->rows as $result) {
 			$customer_group_data[$result['language_id']] = array(
