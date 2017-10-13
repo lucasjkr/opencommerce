4cc4abc989bbb5e4cc8599245fd911261ebc6b27
lucasjkr <lucas@lucaskrupinski.com>
2017-10-13 16:51:44 -0400

    Admin / Model / Customer / Customer_Approval.php is parameterized

--- public_html/admin/model/customer/customer_approval.php
+++ public_html/admin/model/customer/customer_approval.php
@@ -1,26 +1,33 @@
 <?php
 class ModelCustomerCustomerApproval extends Model {
 	public function getCustomerApprovals($data = array()) {
-		$sql = "SELECT *, CONCAT(c.`firstname`, ' ', c.`lastname`) AS name, cgd.`name` AS customer_group, ca.`type` FROM `oc_customer_approval` ca LEFT JOIN `oc_customer` c ON (ca.`customer_id` = c.`customer_id`) LEFT JOIN `oc_customer_group_description` cgd ON (c.`customer_group_id` = cgd.`customer_group_id`) WHERE cgd.`language_id` = '" . (int)$this->config->get('config_language_id') . "'";
+		$sql = "SELECT *, CONCAT(c.`firstname`, ' ', c.`lastname`) AS name, cgd.`name` AS customer_group, ca.`type` FROM `oc_customer_approval` ca LEFT JOIN `oc_customer` c ON (ca.`customer_id` = c.`customer_id`) LEFT JOIN `oc_customer_group_description` cgd ON (c.`customer_group_id` = cgd.`customer_group_id`) WHERE cgd.`language_id` = :language_id";
+
+        $args[':language_id'] = $this->config->get('config_language_id');
 
 		if (!empty($data['filter_name'])) {
-			$sql .= " AND CONCAT(c.`firstname`, ' ', c.`lastname`) LIKE '%" . $this->db->escape((string)$data['filter_name']) . "%'";
+			$sql .= " AND CONCAT(c.`firstname`, ' ', c.`lastname`) LIKE :filter_name";
+            $args[':filter_name'] = '%' . $data['filter_name'] . '%';
 		}
 
 		if (!empty($data['filter_email'])) {
-			$sql .= " AND c.`email` LIKE '" . $this->db->escape((string)$data['filter_email']) . "%'";
+			$sql .= " AND c.`email` LIKE :email";
+            $args[':email'] = $data['filter_email'] . '%';
 		}
 		
 		if (!empty($data['filter_customer_group_id'])) {
-			$sql .= " AND c.`customer_group_id` = '" . (int)$data['filter_customer_group_id'] . "'";
+			$sql .= " AND c.`customer_group_id` = :customer_group_id";
+            $args[':customer_group_id'] = $data['filter_customer_group_id'];
 		}
 		
 		if (!empty($data['filter_type'])) {
-			$sql .= " AND ca.`type` = '" . $this->db->escape((string)$data['filter_type']) . "'";
+			$sql .= " AND ca.`type` = :type";
+            $args[':type'] = $data['filter_type'];
 		}
 		
 		if (!empty($data['filter_date_added'])) {
-			$sql .= " AND DATE(c.`date_added`) = DATE('" . $this->db->escape((string)$data['filter_date_added']) . "')";
+			$sql .= " AND DATE(c.`date_added`) = DATE(:date_added)";
+            $args[':date_added'] = $data['filter_date_added'];
 		}
 
 		$sql .= " ORDER BY c.`date_added` DESC";
@@ -37,13 +44,16 @@ class ModelCustomerCustomerApproval extends Model {
 			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 		}
 
-		$query = $this->db->query($sql);
+		$query = $this->db->query($sql, $args);
 
 		return $query->rows;
 	}
 	
 	public function getCustomerApproval($customer_approval_id) {
-		$query = $this->db->query("SELECT * FROM `oc_customer_approval` WHERE `customer_approval_id` = '" . (int)$customer_approval_id . "'");
+		$query = $this->db->query("SELECT * FROM `oc_customer_approval` WHERE `customer_approval_id` = :customer_approval_id",
+            [
+                ':customer_approval_id' => $customer_approval_id
+            ]);
 		
 		return $query->row;
 	}
@@ -54,41 +64,55 @@ class ModelCustomerCustomerApproval extends Model {
 		$implode = array();
 
 		if (!empty($data['filter_name'])) {
-			$implode[] = "CONCAT(c.`firstname`, ' ', c.`lastname`) LIKE '%" . $this->db->escape((string)$data['filter_name']) . "%'";
+			$implode[] = "CONCAT(c.`firstname`, ' ', c.`lastname`) LIKE :name";
+            $args[':name'] = '%' . $data['filter_name'] . '%';
 		}
 
 		if (!empty($data['filter_email'])) {
-			$implode[] = "c.`email` LIKE '" . $this->db->escape((string)$data['filter_email']) . "%'";
+			$implode[] = "c.`email` LIKE :email";
+            $args[':email'] = $data['filter_email'] . '%'
 		}
 
 		if (!empty($data['filter_customer_group_id'])) {
-			$implode[] = "c.`customer_group_id` = '" . (int)$data['filter_customer_group_id'] . "'";
+			$implode[] = "c.`customer_group_id` = :customer_group_id";
+            $args[':customer_group_id'] = $data['filter_customer_group_id'];
 		}
 		
 		if (!empty($data['filter_type'])) {
-			$implode[] = "ca.`type` = '" . $this->db->escape((string)$data['filter_type']) . "'";
+			$implode[] = "ca.`type` = :type";
+            $args[':type'] = $data['filter_type'];
 		}
 		
 		if (!empty($data['filter_date_added'])) {
-			$implode[] = "DATE(ca.`date_added`) = DATE('" . $this->db->escape((string)$data['filter_date_added']) . "')";
+			$implode[] = "DATE(ca.`date_added`) = DATE(:date_added)";
+            $args[':date_added'] = $data['filter_date_added'];
 		}
 
 		if ($implode) {
 			$sql .= " WHERE " . implode(" AND ", $implode);
 		}
 
-		$query = $this->db->query($sql);
+		$query = $this->db->query($sql, $args);
 
 		return $query->row['total'];
 	}
 	
 	public function approveCustomer($customer_id) {
-		$this->db->query("UPDATE `oc_customer` SET status = '1' WHERE customer_id = '" . (int)$customer_id . "'");
-		$this->db->query("DELETE FROM `oc_customer_approval` WHERE customer_id = '" . (int)$customer_id . "' AND `type` = 'customer'");
+		$this->db->query("UPDATE `oc_customer` SET status = '1' WHERE customer_id = :customer_id",
+            [
+                ':customer_id' => $customer_id
+            ]);
+		$this->db->query("DELETE FROM `oc_customer_approval` WHERE customer_id = :customer_id AND `type` = 'customer'",
+            [
+                ':customer_id' => $customer_id
+            ]);
 	}
 
 	public function denyCustomer($customer_id) {
-		$this->db->query("DELETE FROM `oc_customer_approval` WHERE customer_id = '" . (int)$customer_id . "' AND `type` = 'customer'");
+		$this->db->query("DELETE FROM `oc_customer_approval` WHERE customer_id = :customer_id AND `type` = 'customer'",
+            [
+                ':customer_id' => $customer_id
+            ]);
 	}
 
 }
