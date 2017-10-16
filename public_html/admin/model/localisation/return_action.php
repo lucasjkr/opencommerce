0e1f1fef4ca19098d09a43d4d4a1b0634e56534a
lucasjkr <lucas@lucaskrupinski.com>
2017-10-16 10:13:49 -0400

    two more classes are parameterized

--- public_html/admin/model/localisation/return_action.php
+++ public_html/admin/model/localisation/return_action.php
@@ -3,9 +3,19 @@ class ModelLocalisationReturnAction extends Model {
 	public function addReturnAction($data) {
 		foreach ($data['return_action'] as $language_id => $value) {
 			if (isset($return_action_id)) {
-				$this->db->query("INSERT INTO oc_return_action SET return_action_id = '" . (int)$return_action_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			    // LJK TODO - this first action doesn't look like it could ever get triggered
+				$this->db->query("INSERT INTO `oc_return_action` SET `return_action_id` = :return_action_id, `language_id` = :language_id, `name` = :name",
+                    [
+                        ':return_action_id' => $return_action_id,
+                        ':language_id' => $language_id ,
+                        ':name' => $value['name']
+                    ]);
 			} else {
-				$this->db->query("INSERT INTO oc_return_action SET language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+				$this->db->query("INSERT INTO `oc_return_action` SET `language_id` = :language_id, `name` = :name",
+                    [
+                        ':language_id' => $language_id,
+                        ':name' => $value['name']
+                    ]);
 
 				$return_action_id = $this->db->getLastId();
 			}
@@ -17,32 +27,46 @@ class ModelLocalisationReturnAction extends Model {
 	}
 
 	public function editReturnAction($return_action_id, $data) {
-		$this->db->query("DELETE FROM oc_return_action WHERE return_action_id = '" . (int)$return_action_id . "'");
+		$this->db->query("DELETE FROM `oc_return_action` WHERE `return_action_id` = :return_action_id",
+            [
+                ':return_action_id' => $return_action_id
+            ]);
 
 		foreach ($data['return_action'] as $language_id => $value) {
-			$this->db->query("INSERT INTO oc_return_action SET return_action_id = '" . (int)$return_action_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			$this->db->query("INSERT INTO `oc_return_action` SET `return_action_id` = :return_action_id, `language_id` = :language_id , `name` = :name",
+                [
+                    ':return_action_id' => $return_action_id,
+                    ':language_id' => $language_id,
+                    ':name' => $value['name']
+                ]);
 		}
 
 		$this->cache->delete('return_action');
 	}
 
 	public function deleteReturnAction($return_action_id) {
-		$this->db->query("DELETE FROM oc_return_action WHERE return_action_id = '" . (int)$return_action_id . "'");
+		$this->db->query("DELETE FROM oc_return_action WHERE return_action_id = :return_action_id",
+            [
+                ':return_action_id' => $return_action_id
+            ]);
 
 		$this->cache->delete('return_action');
 	}
 
 	public function getReturnAction($return_action_id) {
-		$query = $this->db->query("SELECT * FROM oc_return_action WHERE return_action_id = '" . (int)$return_action_id . "' AND language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT * FROM oc_return_action WHERE return_action_id = :return_action_id  AND language_id = :language_id",
+            [
+                ':return_action_id' => $return_action_id,
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row;
 	}
 
 	public function getReturnActions($data = array()) {
 		if ($data) {
-			$sql = "SELECT * FROM oc_return_action WHERE language_id = '" . (int)$this->config->get('config_language_id') . "'";
-
-			$sql .= " ORDER BY name";
+			$sql = "SELECT * FROM oc_return_action WHERE language_id = :language_id ORDER BY name";
+            $args[':language_id'] = $this->config->get('config_language_id');
 
 			if (isset($data['order']) && ($data['order'] == 'DESC')) {
 				$sql .= " DESC";
@@ -62,14 +86,17 @@ class ModelLocalisationReturnAction extends Model {
 				$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 			}
 
-			$query = $this->db->query($sql);
+			$query = $this->db->query($sql, $args);
 
 			return $query->rows;
 		} else {
 			$return_action_data = $this->cache->get('return_action.' . (int)$this->config->get('config_language_id'));
 
 			if (!$return_action_data) {
-				$query = $this->db->query("SELECT return_action_id, name FROM oc_return_action WHERE language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY name");
+				$query = $this->db->query("SELECT return_action_id, name FROM oc_return_action WHERE language_id = :language_id ORDER BY name",
+                    [
+                        ':language_id' => $this->config->get('config_language_id')
+                    ]);
 
 				$return_action_data = $query->rows;
 
@@ -83,7 +110,10 @@ class ModelLocalisationReturnAction extends Model {
 	public function getReturnActionDescriptions($return_action_id) {
 		$return_action_data = [];
 
-		$query = $this->db->query("SELECT * FROM oc_return_action WHERE return_action_id = '" . (int)$return_action_id . "'");
+		$query = $this->db->query("SELECT * FROM oc_return_action WHERE return_action_id = :return_action_id",
+            [
+                ':return_action_id' => $return_action_id
+            ]);
 
 		foreach ($query->rows as $result) {
 			$return_action_data[$result['language_id']] = array('name' => $result['name']);
@@ -93,7 +123,10 @@ class ModelLocalisationReturnAction extends Model {
 	}
 
 	public function getTotalReturnActions() {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_return_action WHERE language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_return_action WHERE language_id = :language_id",
+            [
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row['total'];
 	}
--- public_html/admin/model/localisation/return_reason.php
+++ public_html/admin/model/localisation/return_reason.php
@@ -3,9 +3,19 @@ class ModelLocalisationReturnReason extends Model {
 	public function addReturnReason($data) {
 		foreach ($data['return_reason'] as $language_id => $value) {
 			if (isset($return_reason_id)) {
-				$this->db->query("INSERT INTO oc_return_reason SET return_reason_id = '" . (int)$return_reason_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			    // LJK TODO: This first action doesn't look like it will ever get triggered
+				$this->db->query("INSERT INTO oc_return_reason SET return_reason_id = :return_reason_id, language_id = :language_id , name = :name",
+                    [
+                        ':return_reason_id' => $return_reason_id,
+                        ':language_id' => $language_id,
+                        ':name' => $value['name']
+                    ]);
 			} else {
-				$this->db->query("INSERT INTO oc_return_reason SET language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+				$this->db->query("INSERT INTO oc_return_reason SET language_id = :language_id, name = :name",
+                    [
+                        ':language_id' => $language_id,
+                        ':name' => $value['name']
+                    ]);
 
 				$return_reason_id = $this->db->getLastId();
 			}
@@ -17,32 +27,45 @@ class ModelLocalisationReturnReason extends Model {
 	}
 
 	public function editReturnReason($return_reason_id, $data) {
-		$this->db->query("DELETE FROM oc_return_reason WHERE return_reason_id = '" . (int)$return_reason_id . "'");
+		$this->db->query("DELETE FROM oc_return_reason WHERE return_reason_id = :return_reason_id",
+            [
+                ':return_reason_id' => $return_reason_id
+            ]);
 
 		foreach ($data['return_reason'] as $language_id => $value) {
-			$this->db->query("INSERT INTO oc_return_reason SET return_reason_id = '" . (int)$return_reason_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			$this->db->query("INSERT INTO oc_return_reason SET return_reason_id = :return_reason_id, language_id = :language_id, name = :name",
+                [
+                    ':return_reason_id' => $return_reason_id,
+                    ':language_id' => $language_id,
+                    ':name' => $value['name']
+                ]);
 		}
 
 		$this->cache->delete('return_reason');
 	}
 
 	public function deleteReturnReason($return_reason_id) {
-		$this->db->query("DELETE FROM oc_return_reason WHERE return_reason_id = '" . (int)$return_reason_id . "'");
-
+		$this->db->query("DELETE FROM oc_return_reason WHERE return_reason_id =  :return_reason_id",
+            [
+                ':return_reason_id' => $return_reason_id
+            ]);
 		$this->cache->delete('return_reason');
 	}
 
 	public function getReturnReason($return_reason_id) {
-		$query = $this->db->query("SELECT * FROM oc_return_reason WHERE return_reason_id = '" . (int)$return_reason_id . "' AND language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT * FROM oc_return_reason WHERE return_reason_id = :return_reason_id AND language_id = :language_id",
+            [
+                ':return_reason_id' => $return_reason_id,
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row;
 	}
 
 	public function getReturnReasons($data = array()) {
 		if ($data) {
-			$sql = "SELECT * FROM oc_return_reason WHERE language_id = '" . (int)$this->config->get('config_language_id') . "'";
-
-			$sql .= " ORDER BY name";
+			$sql = "SELECT * FROM oc_return_reason WHERE language_id = :language_id ORDER BY name";
+            $args[':language_id'] = $this->config->get('config_language_id');
 
 			if (isset($data['order']) && ($data['order'] == 'DESC')) {
 				$sql .= " DESC";
@@ -62,14 +85,17 @@ class ModelLocalisationReturnReason extends Model {
 				$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 			}
 
-			$query = $this->db->query($sql);
+			$query = $this->db->query($sql, $args);
 
 			return $query->rows;
 		} else {
 			$return_reason_data = $this->cache->get('return_reason.' . (int)$this->config->get('config_language_id'));
 
 			if (!$return_reason_data) {
-				$query = $this->db->query("SELECT return_reason_id, name FROM oc_return_reason WHERE language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY name");
+				$query = $this->db->query("SELECT return_reason_id, name FROM oc_return_reason WHERE language_id = :language_id ORDER BY name",
+                    [
+                        ':language_id' => $this->config->get('config_language_id')
+                    ]);
 
 				$return_reason_data = $query->rows;
 
@@ -83,7 +109,10 @@ class ModelLocalisationReturnReason extends Model {
 	public function getReturnReasonDescriptions($return_reason_id) {
 		$return_reason_data = [];
 
-		$query = $this->db->query("SELECT * FROM oc_return_reason WHERE return_reason_id = '" . (int)$return_reason_id . "'");
+		$query = $this->db->query("SELECT * FROM oc_return_reason WHERE return_reason_id = :return_reason_id",
+            [
+                ':return_reason_id' => $return_reason_id
+            ]);
 
 		foreach ($query->rows as $result) {
 			$return_reason_data[$result['language_id']] = array('name' => $result['name']);
@@ -93,7 +122,10 @@ class ModelLocalisationReturnReason extends Model {
 	}
 
 	public function getTotalReturnReasons() {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_return_reason WHERE language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_return_reason WHERE language_id = :language_id",
+            [
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row['total'];
 	}
