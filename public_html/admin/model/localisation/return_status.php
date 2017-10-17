f6b024cf144693aacdd7180da4fc55af3dc20f24
lucasjkr <lucas@lucaskrupinski.com>
2017-10-17 08:58:20 -0400

    model updates

--- public_html/admin/model/localisation/return_status.php
+++ public_html/admin/model/localisation/return_status.php
@@ -3,9 +3,18 @@ class ModelLocalisationReturnStatus extends Model {
 	public function addReturnStatus($data) {
 		foreach ($data['return_status'] as $language_id => $value) {
 			if (isset($return_status_id)) {
-				$this->db->query("INSERT INTO oc_return_status SET return_status_id = '" . (int)$return_status_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+				$this->db->query("INSERT INTO oc_return_status SET return_status_id = :return_status_id, language_id = :language_id, name = :name",
+                    [
+                        ':return_status_id' => $return_status_id,
+                        ':language_id' => $language_id,
+                        ':name' => $value['name'],
+                    ]);
 			} else {
-				$this->db->query("INSERT INTO oc_return_status SET language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+				$this->db->query("INSERT INTO oc_return_status SET language_id = :language_id, name = :name",
+                    [
+                        ':language_id' => $language_id,
+                        ':name' => $value['name']
+                    ]);
 
 				$return_status_id = $this->db->getLastId();
 			}
@@ -17,32 +26,46 @@ class ModelLocalisationReturnStatus extends Model {
 	}
 
 	public function editReturnStatus($return_status_id, $data) {
-		$this->db->query("DELETE FROM oc_return_status WHERE return_status_id = '" . (int)$return_status_id . "'");
+		$this->db->query("DELETE FROM oc_return_status WHERE return_status_id = :return_status_id",
+            [
+                ':return_status_id' => $return_status_id
+            ]);
 
 		foreach ($data['return_status'] as $language_id => $value) {
-			$this->db->query("INSERT INTO oc_return_status SET return_status_id = '" . (int)$return_status_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			$this->db->query("INSERT INTO oc_return_status SET return_status_id = :return_status_id, language_id = :language_id, name = :name",
+                [
+                    ':return_status_id' => $return_status_id,
+                    ':language_id' => $language_id,
+                    ':name' => $value['name'],
+                ]);
 		}
 
 		$this->cache->delete('return_status');
 	}
 
 	public function deleteReturnStatus($return_status_id) {
-		$this->db->query("DELETE FROM oc_return_status WHERE return_status_id = '" . (int)$return_status_id . "'");
+		$this->db->query("DELETE FROM oc_return_status WHERE return_status_id = :return_status_id",
+            [
+                ':return_status_id' => $return_status_id
+            ]);
 
 		$this->cache->delete('return_status');
 	}
 
 	public function getReturnStatus($return_status_id) {
-		$query = $this->db->query("SELECT * FROM oc_return_status WHERE return_status_id = '" . (int)$return_status_id . "' AND language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT * FROM oc_return_status WHERE return_status_id = :return_status_id  AND language_id = :language_id",
+            [
+                ':return_status_id' => $return_status_id,
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row;
 	}
 
 	public function getReturnStatuses($data = array()) {
 		if ($data) {
-			$sql = "SELECT * FROM oc_return_status WHERE language_id = '" . (int)$this->config->get('config_language_id') . "'";
-
-			$sql .= " ORDER BY name";
+			$sql = "SELECT * FROM oc_return_status WHERE language_id = :language_id ORDER BY name";
+            $args[':language_id'] = $this->config->get('config_language_id');
 
 			if (isset($data['order']) && ($data['order'] == 'DESC')) {
 				$sql .= " DESC";
@@ -62,14 +85,17 @@ class ModelLocalisationReturnStatus extends Model {
 				$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 			}
 
-			$query = $this->db->query($sql);
+			$query = $this->db->query($sql, $args);
 
 			return $query->rows;
 		} else {
 			$return_status_data = $this->cache->get('return_status.' . (int)$this->config->get('config_language_id'));
 
 			if (!$return_status_data) {
-				$query = $this->db->query("SELECT return_status_id, name FROM oc_return_status WHERE language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY name");
+				$query = $this->db->query("SELECT return_status_id, name FROM oc_return_status WHERE language_id = :language_id ORDER BY name",
+                    [
+                        ':language_id' => $this->config->get('config_language_id')
+                    ]);
 
 				$return_status_data = $query->rows;
 
@@ -83,7 +109,10 @@ class ModelLocalisationReturnStatus extends Model {
 	public function getReturnStatusDescriptions($return_status_id) {
 		$return_status_data = [];
 
-		$query = $this->db->query("SELECT * FROM oc_return_status WHERE return_status_id = '" . (int)$return_status_id . "'");
+		$query = $this->db->query("SELECT * FROM oc_return_status WHERE return_status_id = :return_status_id",
+            [
+                ':return_status_id' => $return_status_id
+            ]);
 
 		foreach ($query->rows as $result) {
 			$return_status_data[$result['language_id']] = array('name' => $result['name']);
@@ -93,7 +122,10 @@ class ModelLocalisationReturnStatus extends Model {
 	}
 
 	public function getTotalReturnStatuses() {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_return_status WHERE language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_return_status WHERE language_id = :language_id",
+            [
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row['total'];
 	}
--- public_html/admin/model/localisation/stock_status.php
+++ public_html/admin/model/localisation/stock_status.php
@@ -3,9 +3,18 @@ class ModelLocalisationStockStatus extends Model {
 	public function addStockStatus($data) {
 		foreach ($data['stock_status'] as $language_id => $value) {
 			if (isset($stock_status_id)) {
-				$this->db->query("INSERT INTO oc_stock_status SET stock_status_id = '" . (int)$stock_status_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+				$this->db->query("INSERT INTO oc_stock_status SET stock_status_id = :stock_status_id, language_id = :language_id, name = :name",
+                    [
+                        ':stock_status_id' => $stock_status_id,
+                        ':language_id' => $language_id,
+                        ':name' => $value['name'],
+                    ]);
 			} else {
-				$this->db->query("INSERT INTO oc_stock_status SET language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+				$this->db->query("INSERT INTO oc_stock_status SET language_id = :language_id, name = :name",
+                    [
+                        ':language_id' => $language_id,
+                        ':name' => $value['name'],
+                    ]);
 
 				$stock_status_id = $this->db->getLastId();
 			}
@@ -17,32 +26,46 @@ class ModelLocalisationStockStatus extends Model {
 	}
 
 	public function editStockStatus($stock_status_id, $data) {
-		$this->db->query("DELETE FROM oc_stock_status WHERE stock_status_id = '" . (int)$stock_status_id . "'");
+		$this->db->query("DELETE FROM oc_stock_status WHERE stock_status_id = :stock_status_id",
+            [
+                ':stock_status_id' => $stock_status_id
+            ]);
 
 		foreach ($data['stock_status'] as $language_id => $value) {
-			$this->db->query("INSERT INTO oc_stock_status SET stock_status_id = '" . (int)$stock_status_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			$this->db->query("INSERT INTO oc_stock_status SET stock_status_id = :stock_status_id, language_id = :language_id, name = :name",
+                [
+                    ':stock_status_id' => $stock_status_id,
+                    ':language_id' => $language_id,
+                    ':name' => $value['name'],
+                ]);
 		}
 
 		$this->cache->delete('stock_status');
 	}
 
 	public function deleteStockStatus($stock_status_id) {
-		$this->db->query("DELETE FROM oc_stock_status WHERE stock_status_id = '" . (int)$stock_status_id . "'");
+		$this->db->query("DELETE FROM oc_stock_status WHERE stock_status_id = :stock_status_id",
+            [
+                ':stock_status_id' => $stock_status_id
+            ]);
 
 		$this->cache->delete('stock_status');
 	}
 
 	public function getStockStatus($stock_status_id) {
-		$query = $this->db->query("SELECT * FROM oc_stock_status WHERE stock_status_id = '" . (int)$stock_status_id . "' AND language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT * FROM oc_stock_status WHERE stock_status_id = :stock_status_id AND language_id = :language_id",
+            [
+                ':stock_status_id' => $stock_status_id,
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row;
 	}
 
 	public function getStockStatuses($data = array()) {
 		if ($data) {
-			$sql = "SELECT * FROM oc_stock_status WHERE language_id = '" . (int)$this->config->get('config_language_id') . "'";
-
-			$sql .= " ORDER BY name";
+			$sql = "SELECT * FROM oc_stock_status WHERE language_id = :language_id ORDER BY name";
+            $args[':language_id'] = $this->config->get('config_language_id');
 
 			if (isset($data['order']) && ($data['order'] == 'DESC')) {
 				$sql .= " DESC";
@@ -62,14 +85,17 @@ class ModelLocalisationStockStatus extends Model {
 				$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 			}
 
-			$query = $this->db->query($sql);
+			$query = $this->db->query($sql, $args);
 
 			return $query->rows;
 		} else {
 			$stock_status_data = $this->cache->get('stock_status.' . (int)$this->config->get('config_language_id'));
 
 			if (!$stock_status_data) {
-				$query = $this->db->query("SELECT stock_status_id, name FROM oc_stock_status WHERE language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY name");
+				$query = $this->db->query("SELECT stock_status_id, name FROM oc_stock_status WHERE language_id = :language_id ORDER BY name",
+                    [
+                        ':language_id' => $this->config->get('config_language_id')
+                    ]);
 
 				$stock_status_data = $query->rows;
 
@@ -83,7 +109,10 @@ class ModelLocalisationStockStatus extends Model {
 	public function getStockStatusDescriptions($stock_status_id) {
 		$stock_status_data = [];
 
-		$query = $this->db->query("SELECT * FROM oc_stock_status WHERE stock_status_id = '" . (int)$stock_status_id . "'");
+		$query = $this->db->query("SELECT * FROM oc_stock_status WHERE stock_status_id = :stock_status_id",
+            [
+                ':stock_status_id' => $stock_status_id
+            ]);
 
 		foreach ($query->rows as $result) {
 			$stock_status_data[$result['language_id']] = array('name' => $result['name']);
@@ -93,7 +122,10 @@ class ModelLocalisationStockStatus extends Model {
 	}
 
 	public function getTotalStockStatuses() {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_stock_status WHERE language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_stock_status WHERE language_id = :language_id",
+            [
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->row['total'];
 	}
