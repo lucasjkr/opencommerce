3c4ca653405078f1e695ecbbb2560dcd11f6cd19
lucasjkr <lucas@lucaskrupinski.com>
2017-10-22 10:48:26 -0400

    removed manual setting timestamp

--- common/model/tool/online.php
+++ common/model/tool/online.php
@@ -3,6 +3,6 @@ class ModelToolOnline extends Model {
 	public function addOnline($ip, $customer_id, $url, $referer) {
 		$this->db->query("DELETE FROM `oc_customer_online` WHERE date_added < '" . date('Y-m-d H:i:s', strtotime('-1 hour')) . "'");
 
-		$this->db->query("REPLACE INTO `oc_customer_online` SET `ip` = '" . $this->db->escape($ip) . "', `customer_id` = '" . (int)$customer_id . "', `url` = '" . $this->db->escape($url) . "', `referer` = '" . $this->db->escape($referer) . "', `date_added` = '" . $this->db->escape(date('Y-m-d H:i:s')) . "'");
+		$this->db->query("REPLACE INTO `oc_customer_online` SET `ip` = '" . $this->db->escape($ip) . "', `customer_id` = '" . (int)$customer_id . "', `url` = '" . $this->db->escape($url) . "', `referer` = '" . $this->db->escape($referer) . "'");
 	}
 }
