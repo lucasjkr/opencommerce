5e0c204edf75a510253740beeb7952edb6e0dc0d
lucasjkr <lucas@lucaskrupinski.com>
2017-10-22 10:41:38 -0400

    common / moel / account / customer.php is parameterized

--- common/model/account/customer.php
+++ common/model/account/customer.php
@@ -11,120 +11,229 @@ class ModelAccountCustomer extends Model {
 
 		$customer_group_info = $this->model_account_customer_group->getCustomerGroup($customer_group_id);
 
-		$this->db->query("INSERT INTO oc_customer SET customer_group_id = '" . (int)$customer_group_id . "', store_id = '" . (int)$this->config->get('config_store_id') . "', language_id = '" . (int)$this->config->get('config_language_id') . "', firstname = '" . $this->db->escape((string)$data['firstname']) . "', lastname = '" . $this->db->escape((string)$data['lastname']) . "', email = '" . $this->db->escape((string)$data['email']) . "', telephone = '" . $this->db->escape((string)$data['telephone']) . "', custom_field = '" . $this->db->escape(isset($data['custom_field']['account']) ? json_encode($data['custom_field']['account']) : '') . "', `password` = '" . password_hash($data['password'], PASSWORD_DEFAULT) . "', `newsletter` = '" . (isset($data['newsletter']) ? (int)$data['newsletter'] : 0) . "', `ip` = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "', status = '" . (int)!$customer_group_info['approval'] . "'");
+		$this->db->query("INSERT INTO `oc_customer` SET `customer_group_id` = :customer_group_id, `store_id` = :store_id, `language_id` = :language_id, `firstname` = :firstname, `lastname` = :lastname, `email` = :email, `telephone` = :telephone, `password` = :password, `custom_field` = :custom_field, `newsletter` = :newsletter, `ip` = :ip, status = :status",
+            [
+                ':customer_group_id' => $customer_group_id,
+                ':store_id' => $this->config->get('config_store_id'),
+                ':language_id' => $this->config->get('config_language_id'),
+                ':firstname' => $data['firstname'],
+                ':lastname' => $data['lastname'],
+                ':email' => mb_strtolower($data['email'], 'UTF-8'),
+                ':telephone' => $data['telephone'],
+                ':custom_field' => isset($data['custom_field']['account']) ? json_encode($data['custom_field']['account']) : '',
+                ':password' => password_hash($data['password'], PASSWORD_DEFAULT),
+                ':newsletter' => isset($data['newsletter']) ? (int)$data['newsletter'] : 0,
+                ':ip' => $this->request->server['REMOTE_ADDR'],
+                ':status' => $customer_group_info['approval']
+            ]);
 
 		$customer_id = $this->db->getLastId();
 
 		if ($customer_group_info['approval']) {
-			$this->db->query("INSERT INTO `oc_customer_approval` SET customer_id = '" . (int)$customer_id . "', type = 'customer'");
+			$this->db->query("INSERT INTO `oc_customer_approval` SET `customer_id` = :customer_id, `type` = :type",
+                [
+                    ':customer_id' => $customer_id,
+                    ':type' => 'customer'
+                ]);
 		}
 
 		return $customer_id;
 	}
 
 	public function editCustomer($customer_id, $data) {
-		$this->db->query("UPDATE `oc_customer` SET `firstname` = '" . $this->db->escape((string)$data['firstname']) . "', `lastname` = '" . $this->db->escape((string)$data['lastname']) . "', `email` = '" . $this->db->escape((string)$data['email']) . "', telephone = '" . $this->db->escape((string)$data['telephone']) . "', custom_field = '" . $this->db->escape(isset($data['custom_field']['account']) ? json_encode($data['custom_field']['account']) : '') . "' WHERE customer_id = '" . (int)$customer_id . "'");
+		$this->db->query("UPDATE `oc_customer` SET `firstname` = :firstname, `lastname` = :lastname, `email` = :email, `telephone` = :telephone, custom_field = :custom_field WHERE customer_id = :customer_id ",
+            [
+                ':firstname' => $data['firstname'],
+                ':lastname' => $data['lastname'],
+                ':email' => $data['email'],
+                ':telephone' => $data['telephone'],
+                ':custom_field' => isset($data['custom_field']['account']) ? json_encode($data['custom_field']['account']) : '',
+                ':customer_id' => $customer_id
+            ]);
 	}
 
 	public function editPassword($email, $password) {
-		$this->db->query("UPDATE `oc_customer` password = '" . $this->db->escape(password_hash($password, PASSWORD_DEFAULT)) . "', code = '' WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "'");
+		$this->db->query("UPDATE `oc_customer` SET `password` = :password, `code` = :code WHERE `email` = :email",
+            [
+                ':password' => password_hash($password, PASSWORD_DEFAULT),
+                ':code' => '',
+                ':email' => utf8_strtolower($email),
+            ]);
 	}
 
 	public function editAddressId($customer_id, $address_id) {
-		$this->db->query("UPDATE oc_customer SET address_id = '" . (int)$address_id . "' WHERE customer_id = '" . (int)$customer_id . "'");
+		$this->db->query("UPDATE oc_customer SET address_id = :address_id WHERE customer_id = :customer_id",
+            [
+                ':address_id' => $address_id,
+                ':customer_id' => $customer_id,
+            ]);
 	}
 
 	public function editCode($email, $code) {
-		$this->db->query("UPDATE `oc_customer` SET code = '" . $this->db->escape($code) . "' WHERE LCASE(email) = '" . $this->db->escape(utf8_strtolower($email)) . "'");
+		$this->db->query("UPDATE `oc_customer` SET `code` = :code WHERE `email` = :email",
+            [
+                ':code' => $code,
+                ':email' => utf8_strtolower($email)
+            ]);
 	}
 
 	public function editToken($email, $token) {
-		$this->db->query("UPDATE `oc_customer` SET token = '" . $this->db->escape($token) . "' WHERE LCASE(email) = '" . $this->db->escape(utf8_strtolower($email)) . "'");
+		$this->db->query("UPDATE `oc_customer` SET `token` = :token WHERE `email` = :email",
+            [
+                ':token' => $token,
+                ':email' => utf8_strtolower($email)
+            ]);
 	}
 
 	public function editNewsletter($newsletter) {
-		$this->db->query("UPDATE oc_customer SET newsletter = '" . (int)$newsletter . "' WHERE customer_id = '" . (int)$this->customer->getId() . "'");
+		$this->db->query("UPDATE oc_customer SET newsletter = :newsletter WHERE customer_id = :customer_id",
+            [
+                ':newsletter' => $newsletter,
+                ':customer_id' => $this->customer->getId()
+            ]);
 	}
 
 	public function getCustomer($customer_id) {
-		$query = $this->db->query("SELECT * FROM oc_customer WHERE customer_id = '" . (int)$customer_id . "'");
+		$query = $this->db->query("SELECT * FROM oc_customer WHERE customer_id = :customer_id",
+            [
+                ':customer_id' => $customer_id
+            ]);
 
 		return $query->row;
 	}
 
 	public function getCustomerByEmail($email) {
-		$query = $this->db->query("SELECT * FROM oc_customer WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "'");
+		$query = $this->db->query("SELECT * FROM oc_customer WHERE email = :email",
+            [
+                ':email' => utf8_strtolower($email)
+            ]);
 
 		return $query->row;
 	}
 
 	public function getCustomerByCode($code) {
-		$query = $this->db->query("SELECT customer_id, firstname, lastname, email FROM `oc_customer` WHERE code = '" . $this->db->escape($code) . "' AND code != ''");
+		$query = $this->db->query("SELECT * FROM `oc_customer` WHERE code = :code_1 AND code != :code_2",
+            [
+                ':code_1' => $code,
+                ':code_2' => ''
+            ]);
 
 		return $query->row;
 	}
 
 	public function getCustomerByToken($token) {
-		$query = $this->db->query("SELECT * FROM oc_customer WHERE token = '" . $this->db->escape($token) . "' AND token != ''");
+		$query = $this->db->query("SELECT * FROM oc_customer WHERE token = :token AND token != :token",
+            [
+                ':token_1' => $token,
+                ':token_2' => ''
+            ]);
 
+        // LJK TODO isn't this nullifying the token? Need to look at this more
 		$this->db->query("UPDATE oc_customer SET token = ''");
 
 		return $query->row;
 	}
 
 	public function getTotalCustomersByEmail($email) {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_customer WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "'");
+	    // LJK TODO the premis of this function is flawed - there shouldnt' BE duplicate customers and emails
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_customer WHERE email = :email",
+            [
+                ':email' => utf8_strtolower($email)
+            ]);
 
 		return $query->row['total'];
 	}
 
 	public function addTransaction($customer_id, $description, $amount = '', $order_id = 0) {
-		$this->db->query("INSERT INTO oc_customer_transaction SET customer_id = '" . (int)$customer_id . "', order_id = '" . (float)$order_id . "', description = '" . $this->db->escape($description) . "', amount = '" . (float)$amount . "'");
+		// LJK TODO - Why do we need this - we have an orders table to keep track of this
+	    $this->db->query("INSERT INTO oc_customer_transaction SET customer_id = :customer_id, order_id = :order_id , description = :description , amount = :amount",
+            [
+                ':customer_id' => $customer_id,
+                ':order_id' => $order_id,
+                ':description' => $description,
+                ':amount' => $amount
+            ]);
 	}
 
 	public function deleteTransactionByOrderId($order_id) {
-		$this->db->query("DELETE FROM oc_customer_transaction WHERE order_id = '" . (int)$order_id . "'");
+		$this->db->query("DELETE FROM oc_customer_transaction WHERE order_id = :order_id",
+            [
+                ':order_id' => $order_id
+            ]);
 	}
 
 	public function getTransactionTotal($customer_id) {
-		$query = $this->db->query("SELECT SUM(amount) AS total FROM oc_customer_transaction WHERE customer_id = '" . (int)$customer_id . "'");
+		$query = $this->db->query("SELECT SUM(amount) AS total FROM oc_customer_transaction WHERE customer_id = :customer_id",
+            [
+                ':customer_id' => $customer_id
+            ]);
 
 		return $query->row['total'];
 	}
 
 	public function getTotalTransactionsByOrderId($order_id) {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_customer_transaction WHERE order_id = '" . (int)$order_id . "'");
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_customer_transaction WHERE order_id = :order_id",
+            [
+                ':order_id' => $order_id
+            ]);
 
 		return $query->row['total'];
 	}
 
 	public function getIps($customer_id) {
-		$query = $this->db->query("SELECT * FROM `oc_customer_ip` WHERE customer_id = '" . (int)$customer_id . "'");
+		$query = $this->db->query("SELECT * FROM `oc_customer_ip` WHERE customer_id = :customer_id",
+            [
+                ':customer_id' => $customer_id
+            ]);
 
 		return $query->rows;
 	}
 
 	public function addLogin($customer_id, $ip, $country = '') {
-		$this->db->query("INSERT INTO oc_customer_ip SET customer_id = '" . (int)$customer_id . "', store_id = '" . (int)$this->config->get('config_store_id') . "', ip = '" . $this->db->escape($ip) . "', country = '" . $this->db->escape($country) . "'");
+		$this->db->query("INSERT INTO oc_customer_ip SET customer_id = :customer_id, store_id = :store_id, ip = :ip, country = :country",
+            [
+                ':customer_id' => $customer_id,
+                ':store_id' => $this->config->get('config_store_id'),
+                ':ip' => $ip,
+                ':country' => $country
+            ]);
 	}
 
 	public function addLoginAttempt($email) {
-		$query = $this->db->query("SELECT * FROM oc_customer_login WHERE email = '" . $this->db->escape(utf8_strtolower((string)$email)) . "' AND ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "'");
+		$query = $this->db->query("SELECT * FROM oc_customer_login WHERE email = :email AND ip = :ip",
+            [
+                ':email' => utf8_strtolower($email),
+                ':ip' => $this->request->server['REMOTE_ADDR']
+            ]);
 
 		if (!$query->num_rows) {
-			$this->db->query("INSERT INTO oc_customer_login SET email = '" . $this->db->escape(utf8_strtolower((string)$email)) . "', ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "', total = 1, date_added = '" . $this->db->escape(date('Y-m-d H:i:s')) . "', date_modified = '" . $this->db->escape(date('Y-m-d H:i:s')) . "'");
+			$this->db->query("INSERT INTO oc_customer_login SET email = :email, ip = :ip, total = 1",
+                [
+                    ':email' => utf8_strtolower($email),
+                    ':ip' => $this->request->server['REMOTE_ADDR'],
+
+                ]);
 		} else {
-			$this->db->query("UPDATE oc_customer_login SET total = (total + 1), date_modified = '" . $this->db->escape(date('Y-m-d H:i:s')) . "' WHERE customer_login_id = '" . (int)$query->row['customer_login_id'] . "'");
+			$this->db->query("UPDATE oc_customer_login SET total = (total + 1) WHERE customer_login_id = :customer_login_id",
+                [
+                    ':customer_login_id' => $query->row['customer_login_id']
+                ]);
 		}
 	}
 
 	public function getLoginAttempts($email) {
-		$query = $this->db->query("SELECT * FROM `oc_customer_login` WHERE email = '" . $this->db->escape(utf8_strtolower($email)) . "'");
+		$query = $this->db->query("SELECT * FROM `oc_customer_login` WHERE email = :email",
+            [
+                ':email' => utf8_strtolower($email)
+            ]);
 
 		return $query->row;
 	}
 
 	public function deleteLoginAttempts($email) {
-		$this->db->query("DELETE FROM `oc_customer_login` WHERE email = '" . $this->db->escape(utf8_strtolower($email)) . "'");
+		$this->db->query("DELETE FROM `oc_customer_login` WHERE email = :email",
+            [
+                ':email' => utf8_strtolower($email)
+            ]);
 	}
 }
