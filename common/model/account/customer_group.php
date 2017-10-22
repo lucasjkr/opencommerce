4c169ff439cd64bec059f4855c94407242642b32
lucasjkr <lucas@lucaskrupinski.com>
2017-10-22 11:05:16 -0400

    more models parameterized

--- common/model/account/customer_group.php
+++ common/model/account/customer_group.php
@@ -1,13 +1,20 @@
 <?php
 class ModelAccountCustomerGroup extends Model {
 	public function getCustomerGroup($customer_group_id) {
-		$query = $this->db->query("SELECT DISTINCT * FROM oc_customer_group cg LEFT JOIN oc_customer_group_description cgd ON (cg.customer_group_id = cgd.customer_group_id) WHERE cg.customer_group_id = '" . (int)$customer_group_id . "' AND cgd.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT DISTINCT * FROM oc_customer_group cg LEFT JOIN oc_customer_group_description cgd ON (cg.customer_group_id = cgd.customer_group_id) WHERE cg.customer_group_id = :customer_group_id AND cgd.language_id = :language_id",
+            [
+                ':customer_group_id' => $customer_group_id,
+                ':language_id' => $this->config->get('config_language_id'),
+            ]);
 
 		return $query->row;
 	}
 
 	public function getCustomerGroups() {
-		$query = $this->db->query("SELECT * FROM oc_customer_group cg LEFT JOIN oc_customer_group_description cgd ON (cg.customer_group_id = cgd.customer_group_id) WHERE cgd.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY cg.sort_order ASC, cgd.name ASC");
+		$query = $this->db->query("SELECT * FROM oc_customer_group cg LEFT JOIN oc_customer_group_description cgd ON (cg.customer_group_id = cgd.customer_group_id) WHERE cgd.language_id = :language_id ORDER BY cg.sort_order ASC, cgd.name ASC",
+            [
+                ':language_id' => $this->config->get('config_language_id')
+            ]);
 
 		return $query->rows;
 	}
