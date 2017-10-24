4632bcb3023829908aa1b1225d75e983182d901e
lucasjkr <lucas@lucaskrupinski.com>
2017-10-24 06:33:43 -0400

    delete a file that wasn't used anywhere

--- common/model/checkout/marketing.php
+++
@@ -1,8 +0,0 @@
-<?php
-class ModelCheckoutMarketing extends Model {
-	public function getMarketingByCode($code) {
-		$query = $this->db->query("SELECT * FROM oc_marketing WHERE code = '" . $this->db->escape($code) . "'");
-
-		return $query->row;
-	}
-}
