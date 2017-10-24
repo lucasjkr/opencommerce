c89c07c0154b2d23972a6215627d4b897046d1f4
lucasjkr <lucas@lucaskrupinski.com>
2017-10-24 06:05:18 -0400

    parameterized marketing.php

--- common/model/marketing/coupon_admin.php
+++ common/model/marketing/coupon_admin.php
@@ -1,19 +1,41 @@
 <?php
 class ModelMarketingCouponAdmin extends Model {
 	public function addCoupon($data) {
-		$this->db->query("INSERT INTO oc_coupon SET name = '" . $this->db->escape((string)$data['name']) . "', code = '" . $this->db->escape((string)$data['code']) . "', discount = '" . (float)$data['discount'] . "', type = '" . $this->db->escape((string)$data['type']) . "', total = '" . (float)$data['total'] . "', logged = '" . (int)$data['logged'] . "', shipping = '" . (int)$data['shipping'] . "', date_start = '" . $this->db->escape((string)$data['date_start']) . "', date_end = '" . $this->db->escape((string)$data['date_end']) . "', uses_total = '" . (int)$data['uses_total'] . "', uses_customer = '" . (int)$data['uses_customer'] . "', status = '" . (int)$data['status'] . "'");
+		$this->db->query("INSERT INTO oc_coupon SET name = :name, code = :code, discount = :discount, type = :type, total = :total, logged = :logged, shipping = :shipping, date_start = :date_start, date_end = :date_end, uses_total = :uses_total, uses_customer = :uses_customer, status = :status",
+            [
+                ':name' => $data['name'],
+                ':code' => $data['code'],
+                ':discount' => $data['discount'],
+                ':type' => $data['type'],
+                ':total' => $data['total'],
+                ':logged' => $data['logged'],
+                ':shipping' => $data['shipping'],
+                ':date_start' => $data['date_start'],
+                ':date_end' => $data['date_end'],
+                ':uses_total' => $data['uses_total'],
+                ':uses_customer' => $data['uses_customer'],
+                ':status' => $data['status']
+            ]);
 
 		$coupon_id = $this->db->getLastId();
 
 		if (isset($data['coupon_product'])) {
 			foreach ($data['coupon_product'] as $product_id) {
-				$this->db->query("INSERT INTO oc_coupon_product SET coupon_id = '" . (int)$coupon_id . "', product_id = '" . (int)$product_id . "'");
+				$this->db->query("INSERT INTO oc_coupon_product SET coupon_id = :coupon_id, product_id = :product_id",
+                    [
+                        ':coupon_id' => $coupon_id,
+                        ':product_id' => $product_id
+                    ]);
 			}
 		}
 
 		if (isset($data['coupon_category'])) {
 			foreach ($data['coupon_category'] as $category_id) {
-				$this->db->query("INSERT INTO oc_coupon_category SET coupon_id = '" . (int)$coupon_id . "', category_id = '" . (int)$category_id . "'");
+				$this->db->query("INSERT INTO oc_coupon_category SET coupon_id = :coupon_id, category_id = :category_id",
+                    [
+                        ':coupon_id' => $coupon_id,
+                        ':category_id' => $category_id
+                    ]);
 			}
 		}
 
@@ -21,40 +43,87 @@ class ModelMarketingCouponAdmin extends Model {
 	}
 
 	public function editCoupon($coupon_id, $data) {
-		$this->db->query("UPDATE oc_coupon SET name = '" . $this->db->escape((string)$data['name']) . "', code = '" . $this->db->escape((string)$data['code']) . "', discount = '" . (float)$data['discount'] . "', type = '" . $this->db->escape((string)$data['type']) . "', total = '" . (float)$data['total'] . "', logged = '" . (int)$data['logged'] . "', shipping = '" . (int)$data['shipping'] . "', date_start = '" . $this->db->escape((string)$data['date_start']) . "', date_end = '" . $this->db->escape((string)$data['date_end']) . "', uses_total = '" . (int)$data['uses_total'] . "', uses_customer = '" . (int)$data['uses_customer'] . "', status = '" . (int)$data['status'] . "' WHERE coupon_id = '" . (int)$coupon_id . "'");
+		$this->db->query("UPDATE oc_coupon SET name = :name, code = :code, discount = :discount, type = :type, total = :total, logged = :logged, shipping = :shipping, date_start = :date_start, date_end = :date_end, uses_total = :uses_total, uses_customer = :uses_customer , status = :status WHERE coupon_id = :coupon_id",
+            [
+                ':name' => $data['name'],
+                ':code' => $data['code'],
+                ':discount' => $data['discount'],
+                ':type' => $data['type'],
+                ':total' => $data['total'],
+                ':logged' => $data['logged'],
+                ':shipping' => $data['shipping'],
+                ':date_start' => $data['date_start'],
+                ':date_end' => $data['date_end'],
+                ':uses_total' => $data['uses_total'],
+                ':uses_customer' => $data['uses_customer'],
+                ':status' => $data['status'],
+                ':coupon_id' => $coupon_id
+            ]);
 
-		$this->db->query("DELETE FROM oc_coupon_product WHERE coupon_id = '" . (int)$coupon_id . "'");
+		$this->db->query("DELETE FROM oc_coupon_product WHERE coupon_id = :coupon_id",
+            [
+                ':coupon_id' => $coupon_id
+            ]);
 
 		if (isset($data['coupon_product'])) {
 			foreach ($data['coupon_product'] as $product_id) {
-				$this->db->query("INSERT INTO oc_coupon_product SET coupon_id = '" . (int)$coupon_id . "', product_id = '" . (int)$product_id . "'");
+                $this->db->query("INSERT INTO oc_coupon_product SET coupon_id = :coupon_id, product_id = :product_id",
+                    [
+                        ':coupon_id' => $coupon_id,
+                        ':product_id' => $product_id
+                    ]);
 			}
 		}
 
-		$this->db->query("DELETE FROM oc_coupon_category WHERE coupon_id = '" . (int)$coupon_id . "'");
+		$this->db->query("DELETE FROM oc_coupon_category WHERE coupon_id = :coupon_id",
+            [
+                ':coupon_id' => $coupon_id
+            ]);
 
 		if (isset($data['coupon_category'])) {
 			foreach ($data['coupon_category'] as $category_id) {
-				$this->db->query("INSERT INTO oc_coupon_category SET coupon_id = '" . (int)$coupon_id . "', category_id = '" . (int)$category_id . "'");
+                $this->db->query("INSERT INTO oc_coupon_category SET coupon_id = :coupon_id, category_id = :category_id",
+                    [
+                        ':coupon_id' => $coupon_id,
+                        ':category_id' => $category_id
+                    ]);
 			}
 		}
 	}
 
 	public function deleteCoupon($coupon_id) {
-		$this->db->query("DELETE FROM oc_coupon WHERE coupon_id = '" . (int)$coupon_id . "'");
-		$this->db->query("DELETE FROM oc_coupon_product WHERE coupon_id = '" . (int)$coupon_id . "'");
-		$this->db->query("DELETE FROM oc_coupon_category WHERE coupon_id = '" . (int)$coupon_id . "'");
-		$this->db->query("DELETE FROM oc_coupon_history WHERE coupon_id = '" . (int)$coupon_id . "'");
+		$this->db->query("DELETE FROM oc_coupon WHERE coupon_id = :coupon_id",
+            [
+                ':coupon_id' => $coupon_id
+            ]);
+		$this->db->query("DELETE FROM oc_coupon_product WHERE coupon_id = :coupon_id",
+            [
+                ':coupon_id' => $coupon_id
+            ]);
+		$this->db->query("DELETE FROM oc_coupon_category WHERE coupon_id = :coupon_id",
+            [
+                ':coupon_id' => $coupon_id
+            ]);
+		$this->db->query("DELETE FROM oc_coupon_history WHERE coupon_id = :coupon_id",
+            [
+                ':coupon_id' => $coupon_id
+            ]);
 	}
 
 	public function getCoupon($coupon_id) {
-		$query = $this->db->query("SELECT DISTINCT * FROM oc_coupon WHERE coupon_id = '" . (int)$coupon_id . "'");
+		$query = $this->db->query("SELECT DISTINCT * FROM oc_coupon WHERE coupon_id = :coupon_id",
+            [
+                ':coupon_id' => $coupon_id
+            ]);
 
 		return $query->row;
 	}
 
 	public function getCouponByCode($code) {
-		$query = $this->db->query("SELECT DISTINCT * FROM oc_coupon WHERE code = '" . $this->db->escape($code) . "'");
+		$query = $this->db->query("SELECT DISTINCT * FROM oc_coupon WHERE code = :code",
+            [
+                ':code' => $code
+            ]);
 
 		return $query->row;
 	}
@@ -62,14 +131,14 @@ class ModelMarketingCouponAdmin extends Model {
 	public function getCoupons($data = []) {
 		$sql = "SELECT coupon_id, name, code, discount, date_start, date_end, status FROM oc_coupon";
 
-		$sort_data = array(
+		$sort_data = [
 			'name',
 			'code',
 			'discount',
 			'date_start',
 			'date_end',
 			'status'
-		);
+		];
 
 		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
 			$sql .= " ORDER BY " . $data['sort'];
@@ -103,7 +172,10 @@ class ModelMarketingCouponAdmin extends Model {
 	public function getCouponProducts($coupon_id) {
 		$coupon_product_data = [];
 
-		$query = $this->db->query("SELECT * FROM oc_coupon_product WHERE coupon_id = '" . (int)$coupon_id . "'");
+		$query = $this->db->query("SELECT * FROM oc_coupon_product WHERE coupon_id = :coupon_id",
+            [
+                ':coupon_id' => $coupon_id
+            ]);
 
 		foreach ($query->rows as $result) {
 			$coupon_product_data[] = $result['product_id'];
@@ -115,7 +187,10 @@ class ModelMarketingCouponAdmin extends Model {
 	public function getCouponCategories($coupon_id) {
 		$coupon_category_data = [];
 
-		$query = $this->db->query("SELECT * FROM oc_coupon_category WHERE coupon_id = '" . (int)$coupon_id . "'");
+		$query = $this->db->query("SELECT * FROM oc_coupon_category WHERE coupon_id = :coupon_id",
+            [
+                ':coupon_id' => $coupon_id
+            ]);
 
 		foreach ($query->rows as $result) {
 			$coupon_category_data[] = $result['category_id'];
@@ -139,13 +214,19 @@ class ModelMarketingCouponAdmin extends Model {
 			$limit = 10;
 		}
 
-		$query = $this->db->query("SELECT ch.order_id, CONCAT(c.firstname, ' ', c.lastname) AS customer, ch.amount, ch.date_added FROM oc_coupon_history ch LEFT JOIN oc_customer c ON (ch.customer_id = c.customer_id) WHERE ch.coupon_id = '" . (int)$coupon_id . "' ORDER BY ch.date_added ASC LIMIT " . (int)$start . "," . (int)$limit);
+		$query = $this->db->query("SELECT ch.order_id, CONCAT(c.firstname, ' ', c.lastname) AS customer, ch.amount, ch.date_added FROM oc_coupon_history ch LEFT JOIN oc_customer c ON (ch.customer_id = c.customer_id) WHERE ch.coupon_id = :coupon_id ORDER BY ch.date_added ASC LIMIT " . (int)$start . "," . (int)$limit,
+            [
+                ':coupon_id' => $coupon_id
+            ]);
 
 		return $query->rows;
 	}
 
 	public function getTotalCouponHistories($coupon_id) {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_coupon_history WHERE coupon_id = '" . (int)$coupon_id . "'");
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM oc_coupon_history WHERE coupon_id = :coupone_id",
+            [
+                ':coupon_id' => $coupon_id
+            ]);
 
 		return $query->row['total'];
 	}
