1c136f1241587b9fbbc3f5d75439883d6224a3c7
lucasjkr <lucas@lucaskrupinski.com>
2017-10-26 09:22:09 -0400

    parameterize queries

--- common/model/design/translation.php
+++ common/model/design/translation.php
@@ -3,7 +3,13 @@ class ModelDesignTranslation extends Model {
 	public function getTranslations($route) {
 		$language_code = !empty($this->session->data['language']) ? $this->session->data['language'] : $this->config->get('config_language');
 		
-		$query = $this->db->query("SELECT * FROM oc_translation WHERE store_id = '" . (int)$this->config->get('config_store_id') . "' AND language_id = '" . (int)$this->config->get('config_language_id') . "' AND (route = '" . $this->db->escape($route) . "' OR route = '" . $this->db->escape($language_code) . "')");
+		$query = $this->db->query("SELECT * FROM oc_translation WHERE store_id = :store_id AND language_id = :language_id AND (route = :route_1 OR route = :route_2)",
+            [
+                ':store_id' => $this->config->get('config_store_id'),
+                ':language_id' => $this->config->get('config_language_id'),
+                ':route_1' => $route,
+                ':route_2' => $language_code
+            ]);
 
 		return $query->rows;
 	}
