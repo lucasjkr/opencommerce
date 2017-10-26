a95bf2af2d61aae30c675cd0b9b941b34781e5cd
lucasjkr <lucas@lucaskrupinski.com>
2017-10-26 16:52:06 -0400

    parameterized pp_braintree.php

--- public_html/catalog/controller/extension/payment/pp_braintree.php
+++ public_html/catalog/controller/extension/payment/pp_braintree.php
@@ -580,7 +580,11 @@ class ControllerExtensionPaymentPPBraintree extends Controller {
 
 				$this->session->data['shipping_postcode'] = $this->request->post['details']['shippingAddress']['postalCode'];
 
-				$country_info = $this->db->query("SELECT * FROM `oc_country` WHERE `iso_code_2` = '" . $this->db->escape($this->request->post['details']['shippingAddress']['countryCode']) . "' AND `status` = '1' LIMIT 1")->row;
+				$country_info = $this->db->query("SELECT * FROM `oc_country` WHERE `iso_code_2` = :iso_code_2 AND `status` = :status LIMIT 1",
+                    [
+                        ':iso_code_2' => $this->request->post['details']['shippingAddress']['countryCode'],
+                        ':status' => 1
+                    ])->row;
 
 				if ($country_info) {
 					$this->session->data['guest']['shipping']['country_id'] = $country_info['country_id'];
@@ -601,7 +605,13 @@ class ControllerExtensionPaymentPPBraintree extends Controller {
 						$returned_shipping_zone = '';
 					}
 
-					$zone_info = $this->db->query("SELECT * FROM `oc_zone` WHERE (`name` = '" . $this->db->escape($returned_shipping_zone) . "' OR `code` = '" . $this->db->escape($returned_shipping_zone) . "') AND `status` = '1' AND `country_id` = '" . (int)$country_info['country_id'] . "' LIMIT 1")->row;
+					$zone_info = $this->db->query("SELECT * FROM `oc_zone` WHERE (`name` = :name OR `code` = :code) AND `status` = :status AND `country_id` = :country_id LIMIT 1",
+                        [
+                            ':name' => $returned_shipping_zone,
+                            ':code' => $returned_shipping_zone,
+                            ':status' => 1,
+                            ':country_id' => $country_info['country_id']
+                         ])->row;
 				} else {
 					$this->session->data['guest']['shipping']['country_id'] = '';
 					$this->session->data['guest']['shipping']['country'] = '';
@@ -702,8 +712,18 @@ class ControllerExtensionPaymentPPBraintree extends Controller {
 					unset($shipping_name[0]);
 					$shipping_last_name = implode(' ', $shipping_name);
 
-					$country_info = $this->db->query("SELECT * FROM `oc_country` WHERE `iso_code_2` = '" . $this->db->escape($this->request->post['details']['shippingAddress']['countryCode']) . "' AND `status` = '1' LIMIT 1")->row;
-					$zone_info = $this->db->query("SELECT * FROM `oc_zone` WHERE (`name` = '" . $this->db->escape($this->request->post['details']['shippingAddress']['state']) . "' OR `code` = '" . $this->db->escape($this->request->post['details']['shippingAddress']['state']) . "') AND `status` = '1' AND `country_id` = '" . (int)$country_info['country_id'] . "'")->row;
+					$country_info = $this->db->query("SELECT * FROM `oc_country` WHERE `iso_code_2` = :iso_code_2 AND `status` = :status LIMIT 1",
+                        [
+                            ':iso_code_2' => $this->request->post['details']['shippingAddress']['countryCode'],
+                            ':status' => 1
+                        ])->row;
+					$zone_info = $this->db->query("SELECT * FROM `oc_zone` WHERE (`name` = :name OR `code` = :code) AND `status` = :status AND `country_id` = :country_id",
+                        [
+                            ':name' => $this->request->post['details']['shippingAddress']['state'],
+                            ':code' => $this->request->post['details']['shippingAddress']['state'],
+                            ':status' => 1,
+                            ':country_id' => $country_info['country_id']
+                        ])->row;
 
 					$address_data = array(
 						'firstname'  => $shipping_first_name,
