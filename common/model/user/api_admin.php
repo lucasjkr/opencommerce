b72721e0020e79cd17fc0daeb20d1d9a58dbb3bd
lucasjkr <lucas@lucaskrupinski.com>
2017-10-28 08:09:26 -0400

    more models parameterzed

--- common/model/user/api_admin.php
+++ common/model/user/api_admin.php
@@ -1,14 +1,23 @@
 <?php
 class ModelUserApiAdmin extends Model {
 	public function addApi($data) {
-		$this->db->query("INSERT INTO `oc_api` SET username = '" . $this->db->escape((string)$data['username']) . "', `key` = '" . $this->db->escape((string)$data['key']) . "', status = '" . (int)$data['status'] . "'");
+		$this->db->query("INSERT INTO `oc_api` SET username = :username, `key` = :key, status = :status",
+            [
+                ':username' => $data['username'],
+                ':key' => $data['key'],
+                ':status' => $data['status']
+            ]);
 
 		$api_id = $this->db->getLastId();
 
 		if (isset($data['api_ip'])) {
 			foreach ($data['api_ip'] as $ip) {
 				if ($ip) {
-					$this->db->query("INSERT INTO `oc_api_ip` SET api_id = '" . (int)$api_id . "', ip = '" . $this->db->escape($ip) . "'");
+					$this->db->query("INSERT INTO `oc_api_ip` SET api_id = :api_id, ip = :ip",
+                        [
+                            ':api_id' => $api_id,
+                            ':ip' => $ip
+                        ]);
 				}
 			}
 		}
@@ -17,25 +26,44 @@ class ModelUserApiAdmin extends Model {
 	}
 
 	public function editApi($api_id, $data) {
-		$this->db->query("UPDATE `oc_api` SET username = '" . $this->db->escape((string)$data['username']) . "', `key` = '" . $this->db->escape((string)$data['key']) . "', status = '" . (int)$data['status'] . "' WHERE api_id = '" . (int)$api_id . "'");
+		$this->db->query("UPDATE `oc_api` SET username = :username, `key` = :key, status = :status WHERE api_id = :api_id",
+            [
+                ':username' => $data['username'],
+                ':key' => $data['key'],
+                ':status' => $data['status'],
+                ':api_id' => $api_id
+            ]);
 
-		$this->db->query("DELETE FROM oc_api_ip WHERE api_id = '" . (int)$api_id . "'");
+		$this->db->query("DELETE FROM oc_api_ip WHERE api_id = :api_id",
+            [
+                ':api_id' => $api_id
+            ]);
 
 		if (isset($data['api_ip'])) {
 			foreach ($data['api_ip'] as $ip) {
 				if ($ip) {
-					$this->db->query("INSERT INTO `oc_api_ip` SET api_id = '" . (int)$api_id . "', ip = '" . $this->db->escape($ip) . "'");
+					$this->db->query("INSERT INTO `oc_api_ip` SET api_id = :api_id, ip = :ip",
+                        [
+                            ':api_id' => $api_id,
+                            ':ip' => $ip
+                        ]);
 				}
 			}
 		}
 	}
 
 	public function deleteApi($api_id) {
-		$this->db->query("DELETE FROM `oc_api` WHERE api_id = '" . (int)$api_id . "'");
+		$this->db->query("DELETE FROM `oc_api` WHERE api_id = :api_id",
+            [
+                ':api_id' => $api_id
+            ]);
 	}
 
 	public function getApi($api_id) {
-		$query = $this->db->query("SELECT * FROM `oc_api` WHERE api_id = '" . (int)$api_id . "'");
+		$query = $this->db->query("SELECT * FROM `oc_api` WHERE api_id =  :api_id",
+            [
+                ':api_id' => $api_id
+            ]);
 
 		return $query->row;
 	}
@@ -43,12 +71,12 @@ class ModelUserApiAdmin extends Model {
 	public function getApis($data = []) {
 		$sql = "SELECT * FROM `oc_api`";
 
-		$sort_data = array(
+		$sort_data = [
 			'username',
 			'status',
 			'date_added',
 			'date_modified'
-		);
+        ];
 
 		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
 			$sql .= " ORDER BY " . $data['sort'];
@@ -86,38 +114,66 @@ class ModelUserApiAdmin extends Model {
 	}
 
 	public function addApiIp($api_id, $ip) {
-		$this->db->query("INSERT INTO `oc_api_ip` SET api_id = '" . (int)$api_id . "', ip = '" . $this->db->escape($ip) . "'");
+		$this->db->query("INSERT INTO `oc_api_ip` SET api_id = :api_id, ip = :ip",
+            [
+                ':api_id' => $api_id,
+                ':ip' => $ip
+            ]);
 	}
 
 	public function getApiIps($api_id) {
-		$query = $this->db->query("SELECT * FROM `oc_api_ip` WHERE api_id = '" . (int)$api_id . "'");
+		$query = $this->db->query("SELECT * FROM `oc_api_ip` WHERE api_id = :api_id",
+            [
+                ':api_id' => $api_id
+            ]);
 
 		return $query->rows;
 	}
 
 	public function addApiSession($api_id, $session_id, $ip) {
-		$api_ip_query = $this->db->query("SELECT * FROM `oc_api_ip` WHERE ip = '" . $this->db->escape($ip) . "'");
+		$api_ip_query = $this->db->query("SELECT * FROM `oc_api_ip` WHERE ip = :ip",
+            [
+                ':ip' => $ip
+            ]);
 		
 		if (!$api_ip_query->num_rows) {
-			$this->db->query("INSERT INTO `oc_api_ip` SET api_id = '" . (int)$api_id . "', ip = '" . $this->db->escape($ip) . "'");
+			$this->db->query("INSERT INTO `oc_api_ip` SET api_id = :api_id, ip = :ip",
+                [
+                    ':api_id' => $api_id,
+                    ':ip' => $ip
+                ]);
 		}
  		
-		$this->db->query("INSERT INTO `oc_api_session` SET api_id = '" . (int)$api_id . "', session_id = '" . $this->db->escape($session_id) . "', ip = '" . $this->db->escape($ip) . "'");
+		$this->db->query("INSERT INTO `oc_api_session` SET api_id = :api_id, session_id = :session_id, ip = :ip",
+            [
+                ':api_id' => $api_id,
+                ':session_id' => $session_id,
+                ':ip' => $ip
+            ]);
 
 		return $this->db->getLastId();
 	}
 	
 	public function getApiSessions($api_id) {
-		$query = $this->db->query("SELECT * FROM `oc_api_session` WHERE api_id = '" . (int)$api_id . "'");
+		$query = $this->db->query("SELECT * FROM `oc_api_session` WHERE api_id = :api_id",
+            [
+                ':api_id' => $api_id
+            ]);
 
 		return $query->rows;
 	}
 	
 	public function deleteApiSession($api_session_id) {
-		$this->db->query("DELETE FROM `oc_api_session` WHERE api_session_id = '" . (int)$api_session_id . "'");
+		$this->db->query("DELETE FROM `oc_api_session` WHERE api_session_id = :api_session_id",
+            [
+                ':api_session_id' => $api_session_id
+            ]);
 	}
 	
 	public function deleteApiSessionBySessonId($session_id) {
-		$this->db->query("DELETE FROM `oc_api_session` WHERE session_id = '" . $this->db->escape($session_id) . "'");
+		$this->db->query("DELETE FROM `oc_api_session` WHERE session_id = :session_id",
+            [
+                ':session_id' => $session_id
+            ]);
 	}		
 }
