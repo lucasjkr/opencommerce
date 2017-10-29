cadecad88cf85160c7946803b3f430e380e0444f
lucasjkr <lucas@lucaskrupinski.com>
2017-10-29 09:50:36 -0400

    validation of settings
    SUPPRESS_MAIL, 1 in config.php causes Mail not to check for correct settings, and also prevents it from sending email.

--- system/library/mailer.php
+++ system/library/mailer.php
@@ -25,29 +25,51 @@ class Mailer {
 
     public function __construct() {
         $this->mailer = new PHPMailer\PHPMailer\PHPMailer(true);
+        // if we're set to suppress mail, then we don't need to make sure all of the other settings are correct
+        if(defined('SUPPRESS_MAIL') && SUPPRESS_MAIL == 1) {
+            return;
+        }
+
+        // It would be nice to do more validation of the settings here, but I'm not sure how we'd give useful feedback
+        // since a visitor could trigger an email and therefore see these messages.
+
+        // Could write to log and say "please look at the log for more details"
+
+        if(!defined('SMTP_SERVER')) {
+            throw new Exception('Error: ' . 'SMTP_SERVER not defined in config.php');
+        }
+
+        if(!defined('SMTP_USERNAME')) {
+            throw new Exception('Error: ' . 'SMTP_USERNAME not defined in config.php');
+        }
+
+        if(!defined('SMTP_PASSWORD')) {
+            throw new Exception('Error: ' . 'SMTP_PASSWORD not defined in config.php');
+        }
+
+        if(!defined('SMTP_PORT')) {
+            // LJK TODO: We COULD try to send via port 25, or have a table of known SMTP ports for different service,
+            throw new Exception('Error: ' . 'SMTP_PORT not defined in config.php');
+        }
+
         $this->mailer->isSMTP();
         $this->mailer->isHTML(true);
 
-        // During construction, we should check that the SMTP server stuff was already defined and either throw an error
-        // or exit gracefully depending.
-        $this->mailer->Host = SMTP_SERVER;
-        $this->mailer->Username = SMTP_USERNAME;
-        $this->mailer->Password = SMTP_PASSWORD;
-        $this->mailer->Port = SMTP_PORT;
+        $this->mailer->Host       = SMTP_SERVER;
+        $this->mailer->Username   = SMTP_USERNAME;
+        $this->mailer->Password   = SMTP_PASSWORD;
+        $this->mailer->Port       = SMTP_PORT;
         $this->mailer->SMTPSecure = 'tls';
-        $this->mailer->SMTPAuth = true;
-
+        $this->mailer->SMTPAuth   = true;
     }
 
 
     public function send($email){
         // Turn off emails during development
-        if(!defined('SUPPRESS_SMTP') && SUPPRESS_MAIL == 1) {
+        if(defined('SUPPRESS_MAIL') && SUPPRESS_MAIL == 1) {
             return;
         }
         try {
-            // Need to validate $email
-
             $this->mailer->addAddress($email);
             $this->mailer->addReplyTo(SMTP_REPLYTO, SMTP_NAME);
             $this->mailer->setFrom(SMTP_REPLYTO, SMTP_NAME);
