458a92f4bda507923c4f203f3db6a0edfc699c37
lucasjkr <lucas@lucaskrupinski.com>
2017-10-30 16:28:30 -0400

    Delete old Mail class and rename Mailer to Mail
    Load Mail as part of framework, and store the Mail object in the registry as Mail
    
    Add additional comments to the Mail class, allow sending with empty message bodys (so we can send as text, as HTML and as HTML/text)

--- system/library/mail.php
+++ system/library/mail.php
@@ -1,146 +1,117 @@
 <?php
-/**
- * @package		OpenCart
- * @author		Daniel Kerr
- * @copyright	Copyright (c) 2005 - 2017, OpenCart, Ltd. (https://www.opencart.com/)
- * @license		https://opensource.org/licenses/GPL-3.0
- * @link		https://www.opencart.com
-*/
 
-/**
-* Mail class
-*/
+// This class is nothing more than a wrapper for PHPMailer. Only advantages are:
+// 1 - Less repeated code throughtout the project
+// 2 - Responsibility for development of the mailing code is removed from us.
 
-// THIS CLASS IS NOW REDUNDANT AND WILL BE DELETED SOON.
 class Mail {
-	protected $to;
-	protected $from;
-	protected $sender;
-	protected $reply_to;
-	protected $subject;
-	protected $text;
-	protected $html;
-	protected $attachments = [];
-	public $parameter;
+    protected $subject;
+    protected $text;
+    protected $html;
+    protected $mailer;
 
-	/**
-	 * Constructor
-	 *
-	 * @param	string	$adaptor
-	 *
- 	*/
-	public function __construct($adaptor = 'mail') {
-		$class = 'Mail\\' . $adaptor;
-		
-		if (class_exists($class)) {
-			$this->adaptor = new $class();
-		} else {
-			trigger_error('Error: Could not load mail adaptor ' . $adaptor . '!');
-			exit();
-		}	
-	}
-	
-	/**
-     * 
-     *
-     * @param	mixed	$to
-     */
-	public function setTo($to) {
-		$this->to = $to;
-	}
-	
-	/**
-     * 
-     *
-     * @param	string	$from
-     */
-	public function setFrom($from) {
-		$this->from = $from;
-	}
-	
-	/**
-     * 
-     *
-     * @param	string	$sender
-     */
-	public function setSender($sender) {
-		$this->sender = $sender;
-	}
-	
-	/**
-     * 
-     *
-     * @param	string	$reply_to
-     */
-	public function setReplyTo($reply_to) {
-		$this->reply_to = $reply_to;
-	}
-	
-	/**
-     * 
-     *
-     * @param	string	$subject
-     */
-	public function setSubject($subject) {
-		$this->subject = $subject;
-	}
-	
-	/**
-     * 
-     *
-     * @param	string	$text
-     */
-	public function setText($text) {
-		$this->text = $text;
-	}
-	
-	/**
-     * 
-     *
-     * @param	string	$html
-     */
-	public function setHtml($html) {
-		$this->html = $html;
-	}
-	
-	/**
-     * 
-     *
-     * @param	string	$filename
-     */
-	public function addAttachment($filename) {
-		$this->attachments[] = $filename;
-	}
-	
-	/**
-     * 
-     *
-     */
-	public function send() {
-		if (!$this->to) {
-			throw new \Exception('Error: E-Mail to required!');
-		}
+    public function __get($property)
+    {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
 
-		if (!$this->from) {
-			throw new \Exception('Error: E-Mail from required!');
-		}
+    public function __set($property, $value)
+    {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
 
-		if (!$this->sender) {
-			throw new \Exception('Error: E-Mail sender required!');
-		}
+        return $this;
+    }
 
-		if (!$this->subject) {
-			throw new \Exception('Error: E-Mail subject required!');
-		}
+    public function __construct() {
+        $this->mailer = new PHPMailer\PHPMailer\PHPMailer(true);
 
-		if ((!$this->text) && (!$this->html)) {
-			throw new \Exception('Error: E-Mail message required!');
-		}
-		
-		foreach (get_object_vars($this) as $key => $value) {
-			$this->adaptor->$key = $value;
-		}
-		
-		$this->adaptor->send();
-	}
+//  If we're set to suppress mail, then we don't need to make sure all of the other settings are correct
+        if(defined('SUPPRESS_MAIL') && SUPPRESS_MAIL == 1) {
+            return;
+        }
+//  It would be nice to do more validation of the settings here, but I'm not sure how we'd give useful feedback
+//  since a visitor could trigger an email and therefore see these messages.
+
+//  Could write to log and say "please look at the log for more details"
+        if(!defined('SMTP_SERVER')) {
+            throw new Exception('Error: ' . 'SMTP_SERVER not defined in config.php');
+        }
+
+        if(!defined('SMTP_USERNAME')) {
+            throw new Exception('Error: ' . 'SMTP_USERNAME not defined in config.php');
+        }
+
+        if(!defined('SMTP_PASSWORD')) {
+            throw new Exception('Error: ' . 'SMTP_PASSWORD not defined in config.php');
+        }
+
+        if(!defined('SMTP_PORT')) {
+            throw new Exception('Error: ' . 'SMTP_PORT not defined in config.php');
+        }
+
+//  Perhaps there COULD be an "admin_alerts" table - where alerts here would be written to that table, and a
+//  new module would check that table whenever the Admin user(s) login to the back end?
+
+        $this->mailer->isSMTP();
+        $this->mailer->isHTML(true);
+
+//  Some of OC's messages are formatted as plain text, some as HTML, and some others have both.
+//  Therefore, we use Body and AltBody. If a plain text message is sent (AltBody, but not Body),
+//  then by default, PHPMailer won't allow you to send it. You need to set this override to do so.
+        $this->mailer->AllowEmpty = true;
+
+//  Only enable SMTPDebug in development scenarios
+//        $this->mailer->SMTPDebug = 2;
+
+//  Configure Mail using credentials stored in config.php:
+        $this->mailer->Host       = SMTP_SERVER;
+        $this->mailer->Username   = SMTP_USERNAME;
+        $this->mailer->Password   = SMTP_PASSWORD;
+        $this->mailer->Port       = SMTP_PORT;
+        $this->mailer->SMTPSecure = 'tls';
+        $this->mailer->SMTPAuth   = true;
+    }
+
+    public function setSubject($subject) {
+        $this->subject = $subject;
+    }
+
+    public function setText($text) {
+        $this->text = $text;
+    }
+
+    public function setHtml($html) {
+        $this->html = $html;
+    }
+
+    public function send($email){
+        // Turn off emails during development
+        if(defined('SUPPRESS_MAIL') && SUPPRESS_MAIL == 1) {
+            return true;
+        }
+        try {
+            $this->mailer->addAddress($email);
+            $this->mailer->addReplyTo(SMTP_REPLYTO, SMTP_NAME);
+            $this->mailer->setFrom(SMTP_REPLYTO, SMTP_NAME);
+
+            // Should send or log a warning for the admin if the subject is too long
+            $this->mailer->Subject  = $this->subject;
+            $this->mailer->Body     = $this->html;
+            $this->mailer->AltBody  = $this->text;
+
+            // Sends the email
+            $result = $this->mailer->send();
+
+            // Resets the addresses, so we can send additional emails without including the previous recipient(s)
+            $this->mailer->ClearAddresses();
+
+            return $result;
+        } catch (Exception $e) {
+            echo "Mailer error: " . $this->mailer->ErrorInfo;
+        }
+    }
 }
