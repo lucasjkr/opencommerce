3ed631eb814f29e5bd0e53afde5b74196d475d7e
lucasjkr <lucas@lucaskrupinski.com>
2017-11-01 16:35:25 -0400

    fix errors

--- common/model/setting/event.php
+++ common/model/setting/event.php
@@ -1,7 +1,10 @@
 <?php
 class ModelSettingEvent extends Model {
 	function getEvents() {
-		$query = $this->db->query("SELECT * FROM `oc_event` WHERE `trigger` LIKE 'catalog/%' AND status = '1' ORDER BY `sort_order` ASC");
+		$query = $this->db->query("SELECT * FROM `oc_event` WHERE `trigger` LIKE :trigger AND status = '1' ORDER BY `sort_order` ASC",
+            [
+                ':trigger' => 'catalog/%'
+            ]);
 
 		return $query->rows;
 	}
