297c5f89f0e07d327b9e87ab684981567dfd5646
lucasjkr <lucas@lucaskrupinski.com>
2017-11-03 14:12:28 -0400

    fix error
    Wrong number of parameters

--- common/model/design/banner.php
+++ common/model/design/banner.php
@@ -4,7 +4,6 @@ class ModelDesignBanner extends Model {
 		$query = $this->db->query("SELECT * FROM oc_banner b LEFT JOIN oc_banner_image bi ON (b.banner_id = bi.banner_id) WHERE b.banner_id = :banner_id AND b.status = '1' AND bi.language_id = :language_id ORDER BY bi.sort_order ASC",
             [
                 ':banner_id' => $banner_id,
-                ':status' => 1,
                 ':language_id' => $this->config->get('config_language_id')
             ]);
 		return $query->rows;
